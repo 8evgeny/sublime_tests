0360f006a5b0dfe5c9c2e63e072609e1fa6688df
Evgeny Parubets <8evgeny@gmail.com>
2019-12-05 17:56:33 +0300

    This version 1
    Evgeny Parubets
    
    Please enter the commit message for your changes. Lines starting

---
+++ Current_time.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+ 
+using namespace std;
+
+
+
+ 
+int main ()
+{
+time_t seconds = time(NULL);
+tm* timeinfo = localtime(&seconds);
+cout<<"Current date and time:"<<asctime(timeinfo)<<endl;
+cin.get();
+return 0;
+}
