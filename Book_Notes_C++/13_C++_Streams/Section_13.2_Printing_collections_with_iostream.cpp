0980dce3c3f4090d1e9d2d93e4772fe1212077d1
Parubets Evgeny <parubets.e@alager.ru>
2020-05-31 21:03:23 +0300

    31.05.2020

---
+++ Book_Notes_C++/13_C++_Streams/Section_13.2_Printing_collections_with_iostream.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <algorithm>
+// #include <ostream>
+
+int main(){
+std::cout << std::setprecision(3);
+std::fixed(std::cout);
+
+std::vector<int> v = {1,2,3,4};
+std::copy(v.begin(), v.end(), std::ostream_iterator<float>(std::cout, " ! "));
+std::cout <<'\n';
+
+std::vector<int> v1 = {1,2,3,4,8,16};
+std::boolalpha(std::cout); // print booleans alphabetically
+
+std::transform(v1.begin(), v1.end(), std::ostream_iterator<bool>(std::cout, " "),
+[](int val) {
+return (val % 2) == 0;
+});
+std::cout <<'\n';
+
+std::transform(v1.begin(), v1.end(), std::ostream_iterator<int>(std::cout, " "),
+[](int val) {
+return val * val;
+});
+std::cout <<'\n';
+
+const int N = 10;
+std::generate_n(std::ostream_iterator<int>(std::cout, " "), N, std::rand);
+std::cout <<'\n';
+
+int v2[] = {1,2,3,4,8,16};
+std::transform(v2, std::end(v2), std::ostream_iterator<int>(std::cout, " "),
+[](int val) {
+return val * val;
+});
+std::cout <<'\n';
+
+
+
+}
