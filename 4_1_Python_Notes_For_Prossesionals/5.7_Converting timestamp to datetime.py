14572d08388b9049e5eba2e118ca0048a7e9df7e
Parubets Evgeny <parubets.e@alager.ru>
2020-11-07 00:42:56 +0300

    add

--- 4_1_Python_Notes_For_Prossesionals/5.7_Converting timestamp to datetime.py
+++ 4_1_Python_Notes_For_Prossesionals/5.7_Converting timestamp to datetime.py
@@ -9,7 +9,13 @@ print(seconds_since_epoch)
 utc_date=datetime.utcfromtimestamp(seconds_since_epoch) 
 print(utc_date)
 
+# list_methods = dir(time)
+# for x in list_methods:
+# 	print(x)
 
+list_methods = dir(datetime)
+for x in list_methods:
+	print(x)
 
 
 
