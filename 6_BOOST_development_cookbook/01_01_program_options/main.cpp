d742ac0bf3c0a97471d0b2c7584dbe881b2a3243
Parubets Evgeny <parubets.e@alager.ru>
2020-11-17 23:06:55 +0300

    fix

--- 6_BOOST_development_cookbook/01_01_program_options/main.cpp
+++ 6_BOOST_development_cookbook/01_01_program_options/main.cpp
@@ -23,16 +23,18 @@ desc.add_options()
 //("oranges", opt::value<int>(), "how many oranges do you have"); //простая форма
 // ProgramOptions stores the option value into the variable that is passed by pointer.
 //Here value of "--oranges" option will be stored into 'oranges_var'.
-("oranges,o", opt::value<int>(&oranges_var)->required(), "oranges you have") ("help", "produce help message");
+("oranges,o",opt::value<int>(&oranges_var)->required(),"oranges you have")("help","produce help message")
 
 // 'name' option is not marked with 'required()', so user may not provide it.
-//("name", opt::value<std::string>(), "your name");
+("name", opt::value<std::string>(), "your name");
 
 // Let's parse the command line:
  opt::variables_map vm; // Variable to store our command line arguments.
  opt::store(opt::parse_command_line(argc, argv, desc), vm); // Parsing and storing arguments.
  std::cout << oranges_var << "\n"; //0
  opt::notify(vm);  // Must be called after all the parsing and storing.
+
+// std::cout << vm["name"].as<char>() << "\n";
 
  std::cout << oranges_var << "\n"; //20
  if (vm.count("help")) {
@@ -43,5 +45,5 @@ desc.add_options()
  std::cout << "Fruits count: " << vm["apples"].as<int>() + vm["oranges"].as<int>() << std::endl;
  } // end of `main`
 
-//vm["name"].as<std::string>()<<
+
 
