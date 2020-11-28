221d935942b4ec2eb9f440a3ea92f6b7280254f4
Parubets Evgeny <parubets.e@alager.ru>
2020-11-28 19:27:49 +0300

    1-9

---
+++ Videokurs_1_C++/5/static_assert.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cassert>
+static_assert(sizeof(int) == 4, "platform is ok");
+//static_assert(sizeof(int) == 3);
+
+
+int main(int argc, char **argv) {
+
+
+    return 0;
+}
