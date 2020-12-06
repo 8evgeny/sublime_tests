e224dbd991d2fc2b928423b7ba359783c2cce794
Parubets Evgeny <parubets.e@alager.ru>
2020-12-06 11:13:21 +0300

    +

---
+++ Videokurs_6_C++/C++Starter/002_Storedlnformation/DefaultTypes.cpp
@@ -0,0 +1,18 @@
+// DefaultTypes.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 5;
+	char b;
+	double c;
+
+	cout << b;
+
+    return 0;
+}
+
---
+++ Videokurs_6_C++/C++Starter/002_Storedlnformation/SimpleVariable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	short a = 256;
+
+	cout << a;
+	cout << endl;
+
+	return 0;
+}
+
