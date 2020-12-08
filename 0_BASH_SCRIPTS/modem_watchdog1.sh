315a662b6028cf5d70ccd9470d3dd97e769744f0
Evgeny Parubets <parubets.e@alager.ru>
2020-12-08 18:00:19 +0300

    add

--- 0_BASH_SCRIPTS/modem_watchdog1.sh
+++ 0_BASH_SCRIPTS/modem_watchdog1.sh
@@ -1,32 +1,3 @@
-#!/bin/sh
-# chmod +x /alager/scripts/watchdog_lte.sh
-# echo "* * * * * /alager/scripts/watchdog_lte.sh" | crontab -
-n=0
-while [ 1 ]; do
-  #ping with timeout 10 seconds
-  ping -c 1 -W 10 -w 10 8.8.8.8
-  ret=$?
-  echo ping result $ret
-  if [ $ret -eq 0 ]; then
-    echo ping ok
-    exit 0
-  else
-    echo ping fail
-    n=$((n+1))
-    # when wan-dhcp fail,
-    # net is unreachable and ping return without any delay
-    # using sleep 1 avoid fail count overflow too fast
-    sleep 1
-  fi
-  echo fail counter $n
-  if [ $n -gt 180 ]; then
-    # in case of wan-dhcp fail total time to reboot is 3 min (180 seconds)
-    # in case of ping-timeout total time to reboot is 30 min (1800 seconds)
-    echo 'reboot ec25'
-  fi
-done
-
-
 #!/bin/bash
 
 # MODEM_DEVICE=$1
