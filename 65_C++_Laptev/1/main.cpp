f2862c62a9d3af3775ddd368fd26c075d7054ca2
Parubets Evgeny <parubets.e@alager.ru>
2020-12-12 20:24:00 +0300

    +

--- 65_C++_Laptev/1/main.cpp
+++ 65_C++_Laptev/1/main.cpp
@@ -6,6 +6,15 @@ int main()
 {
     unsigned char nn = 40; // символьная переменная
     cout << nn << ' ' << int(nn) << endl;
+    cout << nn << ' ' << (int)nn << endl;  //Допускается - стиль С
+
+//вывод кодов специальных символов
+    cout << int('\t') << endl; // табуляция
+    cout << int('\n') << endl; // новая строка
+    cout << int('\a') << endl; // звуковой сигнал
+    cout << int('\b') << endl; // backspace
+
+
     return 0;
 
 }
