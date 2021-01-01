bf6fceb1181ec6bbc9a08ffaedb51b9da1b2735d
Parubets Evgeny <parubets.e@alager.ru>
2021-01-01 21:08:08 +0300

    +

---
+++ Videokurs_6_C++/C++Essential/7/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "scopedptr.h"
+#include "scopedptr.cpp"
+#include "cat.h"
+#include <memory>
+#include <exception>
+
+using namespace std;
+
+
+int main()
+{
+
+}
