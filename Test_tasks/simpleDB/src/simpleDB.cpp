1fecc690c62e6807352ddce95c8f346cce2de159
Evgeny Parubets <parubets.e@alager.ru>
2021-01-21 14:29:27 +0300

    +

--- Test_tasks/simpleDB/src/simpleDB.cpp
+++ Test_tasks/simpleDB/src/simpleDB.cpp
@@ -4,16 +4,38 @@ const QString DateTimeFormat = "dd.MM.yyyy hh:mm:ss";
 const QString DateTimeFormat1 = "hh_mm_ss";
 
 int main(int argc, char** argv) {
-    const QString settingsFile = "configDB.ini";
+ const QString settingsFile = "configDB.ini";
+ QSettings settings(settingsFile, QSettings::IniFormat);
+ settings.beginGroup("nameDIR");
+ QString patch_to_DB = settings.value("patch_to_DB").toString();
+ settings.endGroup();
+ vector<string> v;
+ QDir dirDB;
+ dirDB.setPath(patch_to_DB);
+ QStringList listFiles = dirDB.entryList(QDir::Files);
+ fill_vector(listFiles, v);
+ print_vector(v);
 
-    QSettings settings(settingsFile, QSettings::IniFormat);
-    settings.beginGroup("nameDIR");
-    QString patch_to_DB = settings.value("patch_to_DB").toString();
 
 
 
-    return 0;
+
+
+
+
+
+
+
+
+return 0;
 }
 
+void fill_vector(QStringList& listFiles, vector<string>& v) {
+    for (auto& file : listFiles) {
+        v.push_back(file.toStdString());
+    }
+}
 
-
+void print_vector(vector<string>& v) {
+    for (auto element : v)  cout<< element<<'\n';
+}
--- Test_tasks/simpleDB/src/simpleDB.h
+++ Test_tasks/simpleDB/src/simpleDB.h
@@ -27,11 +27,12 @@
 using namespace std;
 
 void fill_vector(QStringList &, vector<string> &);
-void copy_files(vector<QString> &, QString);
-QString ts_to_date(string);
-QString ts_to_date1(string);
-qlonglong date_to_ts(QString);
-QString str_to_qstr(string);
-QString str_to_qstr1(string);
-qlonglong ts_in_string_to_ts_longlomg(string);
-string parsing_ini_file(QString);  //возвращает мин ts
+void print_vector(vector<string>&);
+//void copy_files(vector<QString> &, QString);
+//QString ts_to_date(string);
+//QString ts_to_date1(string);
+//qlonglong date_to_ts(QString);
+//QString str_to_qstr(string);
+//QString str_to_qstr1(string);
+//qlonglong ts_in_string_to_ts_longlomg(string);
+//string parsing_ini_file(QString);  //возвращает мин ts
