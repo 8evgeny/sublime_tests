1046bcfdd02aff50c404ecf0744572f7c663f5f0
Parubets Evgeny <parubets.e@alager.ru>
2021-01-23 01:03:48 +0300

    +

--- Test_tasks/simpleDB/src/simpleDB.h
+++ Test_tasks/simpleDB/src/simpleDB.h
@@ -30,13 +30,6 @@ void print_vector(std::vector<std::string>&);
 void readDB();
 void input_data();
 
-//void copy_files(vector<QString> &, QString);
-//QString ts_to_date(string);
-//QString ts_to_date1(string);
-//qlonglong date_to_ts(QString);
-//QString str_to_qstr(string);
-//QString str_to_qstr1(string);
-//qlonglong ts_in_string_to_ts_longlomg(string);
-//string parsing_ini_file(QString);  //возвращает мин ts
+
 
 
