145496c2edf4569475913f6da4efb74134a463f1
Evgeny Parubets <parubets.e@alager.ru>
2021-02-10 19:17:38 +0300

    +

---
+++ 0_BASH_SCRIPTS/modem_switch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ret=$(ip a |grep "10.64.64.64/32"| wc -l);
+if [ $ret -ne 1 ]
+   then {
+            echo "connection NO!"
+            sudo pkill wvdial
+            sleep 5
+            usb_modeswitch -v 0x12d1 -p 0x14fe -V 0x12d1 -P 0x1506 -M 55534243000000000000000000000011000000000000000000000000000000
+            sleep 5
+            wvdial
+        }
+    else
+	{
+            echo "connection OK!"
+	}
+fi;
+
+
+
+
+
+
+# nano /home/pi/alager/modem_switch.sh
+*/1 * * * *  pi /home/pi/alager/modem_switch.sh
---
+++ 0_BASH_SCRIPTS/ping_arduino.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd  /home/pi/alager/S-852/build/bin
+./ping_arduino
+
