c0ae8848446c12c10c6850ceb50f9a50fc066d9f
Parubets Evgeny <parubets.e@alager.ru>
2021-02-14 09:03:56 +0300

    +

---
+++ Test_11_02_2021/create.cpp
@@ -0,0 +1,11 @@
+#include "main.h"
+int main(int argc, char** argv) {
+  if (argc != 3) {
+    std::cout << "Expected parametr" << std::endl;
+    exit(1);
+  }
+  ParsingFiles parser;
+  parser.ParsingDir(argv[1], argv[2]);
+
+  return 0;
+}
