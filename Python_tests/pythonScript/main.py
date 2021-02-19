ab0a3e8db8e5adc407d74405732e2232b66999a7
Parubets Evgeny <parubets.e@alager.ru>
2021-02-19 23:47:41 +0300

    add

--- Python_tests/pythonScript/main.py
+++ Python_tests/pythonScript/main.py
@@ -3,14 +3,42 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import psycopg2
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+con = psycopg2.connect(
+  database="sadko",
+  user="postgres",
+  password="",
+  host="127.0.0.1",
+  port="5432"
+)
+
+print("Database opened successfully")
+cur = con.cursor()
+cur.execute("SELECT * FROM violation_order LIMIT 2")
+
+rows = cur.fetchall()
+for row in rows:
+    print("uid =", row[0])
+    print("NAME =", row[1])
+    print("AGE =", row[2])
+    print("COURSE =", row[3])
+    print("DEPARTMENT =", row[4], "\n")
+
+print("Operation done successfully")
+con.close()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# import psycopg2
+# # conn = psycopg2.connect(dbname='sadko', user='postgres',password='', host='localhost')
+# conn = psycopg2.connect(dbname='sadko', user='postgres')
+# cursor = conn.cursor()
+# cursor.execute('SELECT * FROM violation_order LIMIT 10')
+# records = cursor.fetchall()
+# for row in cursor:
+#     print(row)
+# cursor.fetchall()
+# cursor.close()
+# conn.close()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
