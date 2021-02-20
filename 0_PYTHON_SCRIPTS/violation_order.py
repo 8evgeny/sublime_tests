5591b5e9b15bef4b50eb5782a0b866affbbc5614
Parubets Evgeny <parubets.e@alager.ru>
2021-02-20 12:35:08 +0300

    ++

--- 0_PYTHON_SCRIPTS/violation_order.py
+++ 0_PYTHON_SCRIPTS/violation_order.py
@@ -1,11 +1,15 @@
+from datetime import datetime
+time = datetime.now()
+print("Текущее время: ",time)
 path_to_write = "/home/jhon/Desktop/"
-# print ("Введите код нарушения (0 - проезды):")
-# code_input = input()
-code_input = 0
+print ("Введите код нарушения :")
+code_violation = input()
 # print ("Введите адрес хоста")
 # host = input()
 host = "localhost"
+
 import psycopg2
+
 try:
     connection = psycopg2.connect(
       database="sadko",
@@ -14,32 +18,57 @@ try:
       host=host,
       port="5432"
     )
-    print("Подключение к базе успешно\n")
     cur = connection.cursor()
-    count = "SELECT count(*) from violation_order where violation_code  = " + str(code_input)
-    cur.execute(count)
-    row = cur.fetchall()
-    print("Записей в базе данных с кодом ",code_input," - ", row[0])
+    print("Подключение к базе успешно\n")
 
-    reqest = '''
+    count_passages = "SELECT count(*) from violation_order where violation_code  = 0"
+    cur.execute(count_passages)
+    row = cur.fetchall()
+    print("В базе данных проездов -", str(row[0])[1:-2])
+
+    count_violations = "SELECT count(*) from violation_order where violation_code  = " + str(code_violation)
+    cur.execute(count_violations)
+    row = cur.fetchall()
+    print("В базе данных нарушений с кодом",code_violation," -", str(row[0])[1:-2])
+
+    reqest_passages = '''
+    SELECT
+    uid,fixation_timestamp
+    FROM violation_order
+    where violation_code  = 0
+    ORDER BY fixation_timestamp 
+    '''
+    reqest_violation = '''
     SELECT
     uid,fixation_timestamp
     FROM violation_order
     where violation_code  = 
     '''\
-    + str(code_input) + \
+    + str(code_violation) + \
     '''
     ORDER BY fixation_timestamp 
     '''
-    cur.execute(reqest)
+    name1 = path_to_write + str(time.year) + "_" + str(time.month) + "_" + \
+            str(time.day) + "_" + str(time.hour) + str(time.minute) + \
+            str(time.second) + "_" + "passages.csv"
+    name2 = path_to_write + str(time.year) + "_" + str(time.month) + "_" + \
+            str(time.day) + "_" + str(time.hour) + str(time.minute) + \
+            str(time.second) + "_" + "violations_code" + str(code_violation)+".csv"
+    cur.execute(reqest_passages)
     rows = cur.fetchall()
-    # filename=
-    file = open(path_to_write+"newfile.txt", "w")
-
-    file.write("\n")
+    file = open(name1, "w")
     for row in rows:
-        file.write(str(row[0])+'\t\t\t\t\t'+str(row[1])+'\n')
-    print("Результаты запроса сохранены в файл: ",path_to_write+"newfile.txt")
+        file.write(str(row[1])[0:10]+";  "+str(row[1])[11:19]+';'+'  '+str(row[0])[0:36]+';'+'\n')
+    file.close()
+
+    cur.execute(reqest_violation)
+    rows = cur.fetchall()
+    file = open(name2, "w")
+    for row in rows:
+        file.write(str(row[1])[0:10]+";  "+str(row[1])[11:19]+';'+'  '+str(row[0])[0:36]+';'+'\n')
+    file.close()
+    print("Результаты запроса по проездам сохранены в файл: ", name1)
+    print("Результаты запроса по нарушениям сохранены в файл: ", name2)
 except psycopg2.Error as e:
         print("Ошибка запроса:\n", e.args[0])
 else:
--- Python_tests/pythonScript/main.py
+++ Python_tests/pythonScript/main.py
@@ -1,11 +1,17 @@
-path_to_write = "/home/jhon/Desktop/"
-print ("Введите код нарушения :")
+path_to_write = "/home/jhon/Desktop/results/"
+
+from datetime import datetime
+time = datetime.now()
+print("Текущее время: ",time)
+# print ("Введите код нарушения :")
 # code_violation = input()
 code_violation = 0
-print ("Введите адрес хоста")
+# print ("Введите адрес хоста")
 # host = input()
 host = "localhost"
+
 import psycopg2
+
 try:
     connection = psycopg2.connect(
       database="sadko",
@@ -16,24 +22,31 @@ try:
     )
     cur = connection.cursor()
     print("Подключение к базе успешно\n")
+# Получаем коды нарушений
 
-    count_passages = "SELECT count(*) from violation_order where violation_code  = 0"
-    cur.execute(count_passages)
-    row = cur.fetchall()
-    print("В базе данных проездов -", str(row[0])[1:-2])
+    codes = "SELECT DISTINCT violation_code from violation_order"
+    cur.execute(codes)
+    code = cur.fetchall()
+
+
+
+    # count_passages = "SELECT count(*) from violation_order where violation_code  = 0"
+    # cur.execute(count_passages)
+    # row = cur.fetchall()
+    # print("В базе данных проездов -", str(row[0])[1:-2])
 
     count_violations = "SELECT count(*) from violation_order where violation_code  = " + str(code_violation)
     cur.execute(count_violations)
     row = cur.fetchall()
     print("В базе данных нарушений с кодом",code_violation," -", str(row[0])[1:-2])
 
-    reqest_passages = '''
-    SELECT
-    uid,fixation_timestamp
-    FROM violation_order
-    where violation_code  = 0
-    ORDER BY fixation_timestamp 
-    '''
+    # reqest_passages = '''
+    # SELECT
+    # uid,fixation_timestamp
+    # FROM violation_order
+    # where violation_code  = 0
+    # ORDER BY fixation_timestamp
+    # '''
     reqest_violation = '''
     SELECT
     uid,fixation_timestamp
@@ -44,22 +57,30 @@ try:
     '''
     ORDER BY fixation_timestamp 
     '''
-
-    cur.execute(reqest_passages)
-    rows = cur.fetchall()
-    file = open(path_to_write+"passages.csv", "w")
-    for row in rows:
-        file.write(str(row[1])[0:10]+";  "+str(row[1])[11:19]+';'+'  '+str(row[0])[0:36]+';'+'\n')
-    file.close()
+    # name1 = path_to_write + str(time.year) + "." + str(time.month) + "." + \
+    #         str(time.day) + "_" + str(time.hour) + "." + str(time.minute) + "." + \
+    #         str(time.second) + "_" + "passages.csv"
+    name = path_to_write + str(time.year) + "." + str(time.month) + "." + \
+            str(time.day) + "_" + str(time.hour) + "." + str(time.minute) + "." + \
+            str(time.second) + "_" + "violations_code" + str(code_violation)+".csv"
+    # cur.execute(reqest_passages)
+    # rows = cur.fetchall()
+    # file = open(name1, "w")
+    # for row in rows:
+    #     file.write(str(row[1])[0:10]+";  "+str(row[1])[11:19]+';'+'  '+str(row[0])[0:36]+';'+'\n')
+    # file.close()
 
     cur.execute(reqest_violation)
     rows = cur.fetchall()
-    file = open(path_to_write+"violations.csv", "w")
+    file = open(name, "w")
     for row in rows:
         file.write(str(row[1])[0:10]+";  "+str(row[1])[11:19]+';'+'  '+str(row[0])[0:36]+';'+'\n')
     file.close()
-    print("Результаты запроса по проездам сохранены в файл: ",path_to_write+"passages.csv")
-    print("Результаты запроса по нарушениям сохранены в файл: ", path_to_write + "violations.csv")
+    # print("Результаты запроса по проездам сохранены в файл: ", name1)
+    print("Результаты запроса по нарушениям сохранены в файл: ", name)
+
+
+
 except psycopg2.Error as e:
         print("Ошибка запроса:\n", e.args[0])
 else:
