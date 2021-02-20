fe8efdf9510e8fe5b0def300fa609d148fc4d810
Parubets Evgeny <parubets.e@alager.ru>
2021-02-20 12:59:46 +0300

    finish script

--- Python_tests/pythonScript/main.py
+++ Python_tests/pythonScript/main.py
@@ -2,16 +2,12 @@ path_to_write = "/home/jhon/Desktop/results/"
 
 from datetime import datetime
 time = datetime.now()
-print("Текущее время: ",time)
-# print ("Введите код нарушения :")
-# code_violation = input()
 code_violation = 0
-# print ("Введите адрес хоста")
-# host = input()
-host = "localhost"
+print ("\n\nВведите адрес хоста")
+host = input()
+# host = "localhost"
 
 import psycopg2
-
 try:
     connection = psycopg2.connect(
       database="sadko",
@@ -21,66 +17,36 @@ try:
       port="5432"
     )
     cur = connection.cursor()
-    print("Подключение к базе успешно\n")
-# Получаем коды нарушений
-
+    print("Подключение к комплексу "+str(host)+" - OK.\n")
     codes = "SELECT DISTINCT violation_code from violation_order"
     cur.execute(codes)
-    code = cur.fetchall()
+    codes_in_DB = cur.fetchall()
+    for code in codes_in_DB:
+        count_violations = "SELECT count(*) from violation_order where violation_code  = " + str(code)[1:-2]
+        cur.execute(count_violations)
+        row = cur.fetchall()
+        print("В базе данных нарушений с кодом",str(code)[1:-2]," -", str(row[0])[1:-2])
 
-
-
-    # count_passages = "SELECT count(*) from violation_order where violation_code  = 0"
-    # cur.execute(count_passages)
-    # row = cur.fetchall()
-    # print("В базе данных проездов -", str(row[0])[1:-2])
-
-    count_violations = "SELECT count(*) from violation_order where violation_code  = " + str(code_violation)
-    cur.execute(count_violations)
-    row = cur.fetchall()
-    print("В базе данных нарушений с кодом",code_violation," -", str(row[0])[1:-2])
-
-    # reqest_passages = '''
-    # SELECT
-    # uid,fixation_timestamp
-    # FROM violation_order
-    # where violation_code  = 0
-    # ORDER BY fixation_timestamp
-    # '''
-    reqest_violation = '''
-    SELECT
-    uid,fixation_timestamp
-    FROM violation_order
-    where violation_code  = 
-    '''\
-    + str(code_violation) + \
-    '''
-    ORDER BY fixation_timestamp 
-    '''
-    # name1 = path_to_write + str(time.year) + "." + str(time.month) + "." + \
-    #         str(time.day) + "_" + str(time.hour) + "." + str(time.minute) + "." + \
-    #         str(time.second) + "_" + "passages.csv"
-    name = path_to_write + str(time.year) + "." + str(time.month) + "." + \
+        reqest_violation = '''
+        SELECT
+        uid,fixation_timestamp
+        FROM violation_order
+        where violation_code  = 
+        '''\
+        + str(code)[1:-2] + \
+        '''
+        ORDER BY fixation_timestamp 
+        '''
+        name = path_to_write + str(time.year) + "." + str(time.month) + "." + \
             str(time.day) + "_" + str(time.hour) + "." + str(time.minute) + "." + \
-            str(time.second) + "_" + "violations_code" + str(code_violation)+".csv"
-    # cur.execute(reqest_passages)
-    # rows = cur.fetchall()
-    # file = open(name1, "w")
-    # for row in rows:
-    #     file.write(str(row[1])[0:10]+";  "+str(row[1])[11:19]+';'+'  '+str(row[0])[0:36]+';'+'\n')
-    # file.close()
-
-    cur.execute(reqest_violation)
-    rows = cur.fetchall()
-    file = open(name, "w")
-    for row in rows:
-        file.write(str(row[1])[0:10]+";  "+str(row[1])[11:19]+';'+'  '+str(row[0])[0:36]+';'+'\n')
-    file.close()
-    # print("Результаты запроса по проездам сохранены в файл: ", name1)
-    print("Результаты запроса по нарушениям сохранены в файл: ", name)
-
-
-
+            str(time.second) + "_" + "violations_code" + str(code)[1:-2]+".csv"
+        cur.execute(reqest_violation)
+        rows = cur.fetchall()
+        file = open(name, "w")
+        for row in rows:
+            file.write(str(row[1])[0:10]+";  "+str(row[1])[11:19]+';'+'  '+str(row[0])[0:36]+';'+'\n')
+        file.close()
+        print("Результаты сохранены в файл: ", name)
 except psycopg2.Error as e:
         print("Ошибка запроса:\n", e.args[0])
 else:
