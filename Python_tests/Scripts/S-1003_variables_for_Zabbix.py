acd95533c6b479ae258db9e0e3a0daaa4c7973f0
Parubets Evgeny <parubets.e@alager.ru>
2021-02-24 23:04:34 +0300

    ++

---
+++ Python_tests/Scripts/S-1003_variables_for_Zabbix.py
@@ -0,0 +1,9 @@
+path_to_write = "lists_violations/"
+from datetime import datetime,timedelta
+time = datetime.now()
+print ("\n\nВведите адрес хоста")
+host = input()
+# host = "localhost"
+# host = "10.8.1.93"
+
+
