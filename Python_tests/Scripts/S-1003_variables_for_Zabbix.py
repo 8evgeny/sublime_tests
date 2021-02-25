54d81e6fc2646ce3a65970062d2a10b41012b78b
Evgeny Parubets <parubets.e@alager.ru>
2021-02-25 14:00:49 +0300

    finish

--- Python_tests/Scripts/S-1003_variables_for_Zabbix.py
+++ Python_tests/Scripts/S-1003_variables_for_Zabbix.py
@@ -15,17 +15,17 @@ v4 = "kris_passages_bck"
 v5 = "db_violations_total"
 v6 = "db_passages_total"
 
-zabbix_path = "/home/eparubets/tmp/zabbix_shooter/"
-# zabbix_path = "/home/tmp/zabbix_shooter/"
+# zabbix_path = "/home/eparubets/tmp/zabbix_shooter/"
+zabbix_path = "/home/tmp/zabbix_shooter/"
 
-path_1 = '/home/eparubets/codd/Sync/v1'
-# path_1 = '/home/codd/Sync/violation'
-path_2 = '/home/eparubets/codd/Sync/v2'
-# path_2 = '/home/codd/Sync/.stversions/violation'
-path_3 = '/home/eparubets/codd/Sync/v3'
-# path_3 = ' /alager/sadko/build/data/kris/passage'
-path_4 = '/home/eparubets/codd/Sync/v4'
-# path_4 = /alager/sadko/build/data/kris/
+# path_1 = '/home/eparubets/codd/Sync/v1'
+path_1 = '/home/codd/Sync/violation'
+# path_2 = '/home/eparubets/codd/Sync/v2'
+path_2 = '/home/codd/Sync/.stversions/violation'
+# path_3 = '/home/eparubets/codd/Sync/v3'
+path_3 = '/alager/sadko/build/data/kris/passage'
+# path_4 = '/home/eparubets/codd/Sync/v4'
+path_4 = '/alager/sadko/build/data/kris/'
 
 from datetime import datetime,timedelta
 time = datetime.now()
@@ -58,17 +58,27 @@ try:
       port="5432"
     )
     cur = connection.cursor()
-    print("\nПодключение к БД - OK.")
+    # print("\nподключение к БД - OK.")
 
     count_violations = \
     "SELECT count(*) FROM violation_order "\
     "WHERE violation_code  > 0 "
     cur.execute(count_violations)
     row = cur.fetchall()
-    print("Всего нарушений в базе -", str(row[0])[1:-2])
-
+    print("нарушений в базе -", str(row[0])[1:-2])
+    file = open(zabbix_path + v5, "w")
+    file.write(str(row[0])[1:-2])
+    file.close()
+    count_passeges = \
+    "SELECT count(*) FROM violation_order "\
+    "WHERE violation_code  = 0 "
+    cur.execute(count_passeges)
+    row = cur.fetchall()
+    print("проездов в базе -", str(row[0])[1:-2])
+    file = open(zabbix_path + v6, "w")
+    file.write(str(row[0])[1:-2])
+    file.close()
 except psycopg2.Error as e:
         print("Ошибка запроса:\n", e.args[0])
 else:
-    # connection.commit()
     connection.close()
