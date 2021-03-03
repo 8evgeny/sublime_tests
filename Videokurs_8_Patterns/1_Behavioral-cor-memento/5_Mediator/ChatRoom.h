18839c6d614daf1904f90ce29e05ca468bb37281
Parubets Evgeny <parubets.e@alager.ru>
2021-03-03 23:50:01 +0300

    Pattern

---
+++ Videokurs_8_Patterns/1_Behavioral-cor-memento/5_Mediator/ChatRoom.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+struct ChatRoom
+{
+  vector<Person> people;
+
+  class PersonReference
+  {
+    vector<Person>& people;
+    unsigned int index;
+  public:
+
+    PersonReference(vector<Person>& persons, const unsigned index)
+      : people(persons),
+        index(index)
+    {
+    }
+
+    Person* operator->() const;
+  };
+
+  void broadcast(const string& origin, const string& message);
+  PersonReference join(Person&& p);
+  void message(const string& origin, const string& who, const string& message);
+};
---
+++ Videokurs_8_Patterns/1_Behavioral-cor-memento/5_Mediator/chat.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include "person.h"
+#include "chatroom.h"
+
+int main_()
+{
+  ChatRoom room;
+
+  auto john = room.join(Person{ "john" });
+  auto jane = room.join(Person{ "jane" });
+
+  john->say("hi room");
+  jane->say("oh, hey john");
+
+  auto simon = room.join(Person{ "simon" });
+  simon->say("hi everyone!");
+
+  jane->pm("simon", "glad you could join us, simon");
+
+  getchar();
+  return 0;
+}
