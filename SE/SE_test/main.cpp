9f7791e8565f70affc11af315913f92ea14ba75b
Parubets Evgeny <parubets.e@alager.ru>
2021-04-02 00:32:43 +0300

    +

--- SE/SE_test/main.cpp
+++ SE/SE_test/main.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main(int argc, char** argv) {
   nativ curr_Time;
   nativ my("Evgeny");
-  //  nativ t("Танюша");
-  //  nativ d1("Stasya");
-  //  nativ d2("Taya");
-  //  nativ d3("Maya");
-  //  nativ d4("Ulyana");
+  nativ t("Танюша");
+  nativ d1("Stasya");
+  nativ d2("Taya");
+  nativ d3("Maya");
+  nativ d4("Ulyana");
 
   //  muhurta mux;
   //  muhurta my_mux(my);
@@ -21,7 +21,12 @@ int main(int argc, char** argv) {
 
   //  thread t1(&nativ::Qtvara, ref(curr_Time));
   //  thread t2(&nativ::Qtvara, ref(t));
-  cout << "navatara: " << my.navatara(curr_Time) << endl;
+  cout << "navatara: " << my.name << " " << my.navatara(curr_Time) << endl;
+  cout << "navatara: " << t.name << " " << t.navatara(curr_Time) << endl;
+  cout << "navatara: " << d1.name << " " << d1.navatara(curr_Time) << endl;
+  cout << "navatara: " << d2.name << " " << d2.navatara(curr_Time) << endl;
+  cout << "navatara: " << d3.name << " " << d3.navatara(curr_Time) << endl;
+  cout << "navatara: " << d4.name << " " << d4.navatara(curr_Time) << endl;
   curr_Time.qtPanchang(argc, argv);
   //  t1.join();
   //  t2.join();
