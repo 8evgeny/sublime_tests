efef56023690a9c4f863006898a3209946da890e
Evgeny Parubets <parubets.e@alager.ru>
2021-04-27 11:18:51 +0300

    add

--- 6_BOOST_development_cookbook/05_03_mutex/main.cpp
+++ 6_BOOST_development_cookbook/05_03_mutex/main.cpp
@@ -11,44 +11,44 @@ namespace without_sync {
 int shared_i = 0;
 
 void do_inc() {
-    for (std::size_t i = 0; i < 30000; ++i) {
-        // do some work
-        // ...
-        int i_snapshot = ++ shared_i;
+  for (std::size_t i = 0; i < 30000; ++i) {
+    // do some work
+    // ...
+    int i_snapshot = ++shared_i;
 
-        // do some work with i_snapshot
-        // ...
-        (void) i_snapshot;
-    }
+    // do some work with i_snapshot
+    // ...
+    (void)i_snapshot;
+  }
 }
 
 void do_dec() {
-    for (std::size_t i = 0; i < 30000; ++i) {
-        // do some work
-        // ...
-        int i_snapshot = -- shared_i;
+  for (std::size_t i = 0; i < 30000; ++i) {
+    // do some work
+    // ...
+    int i_snapshot = --shared_i;
 
-        // do some work with i_snapshot
-        // ...
-        (void) i_snapshot;
-    }
+    // do some work with i_snapshot
+    // ...
+    (void)i_snapshot;
+  }
 }
 
 void run() {
-    boost::thread t1(&do_inc);
-    boost::thread t2(&do_dec);
+  boost::thread t1(&do_inc);
+  boost::thread t2(&do_dec);
 
-    t1.join();
-    t2.join();
+  t1.join();
+  t2.join();
 
-    // assert(global_i == 0); // Oops!
-    std::cout << "shared_i == " << shared_i;
+  // assert(global_i == 0); // Oops!
+  std::cout << "shared_i == " << shared_i;
 }
 
-} // namespace without_sync
+}  // namespace without_sync
 
-#include <boost/thread/mutex.hpp>
 #include <boost/thread/locks.hpp>
+#include <boost/thread/mutex.hpp>
 
 namespace with_sync {
 
@@ -56,55 +56,55 @@ int shared_i = 0;
 boost::mutex i_mutex;
 
 void do_inc() {
-    for (std::size_t i = 0; i < 30000; ++i) {
-        // do some work
-        // ...
-        int i_snapshot;
-        { // Critical section begin
-            boost::lock_guard<boost::mutex> lock(i_mutex);
-            i_snapshot = ++ shared_i;
-        } // Critical section end
+  for (std::size_t i = 0; i < 30000; ++i) {
+    // do some work
+    // ...
+    int i_snapshot;
+    {  // Critical section begin
+      boost::lock_guard<boost::mutex> lock(i_mutex);
+      i_snapshot = ++shared_i;
+    }  // Critical section end
 
-        // do some work with i_snapshot
-        // ...
-        (void)i_snapshot;
-    }
+    // do some work with i_snapshot
+    // ...
+    (void)i_snapshot;
+  }
 }
 
 void do_dec() {
-    for (std::size_t i = 0; i < 30000; ++i) {
-        // do some work
-        // ...
-        int i_snapshot;
-        { // Critical section begin
-            boost::lock_guard<boost::mutex> lock(i_mutex);
-            i_snapshot = -- shared_i;
-        } // Critical section end
+  for (std::size_t i = 0; i < 30000; ++i) {
+    // do some work
+    // ...
+    int i_snapshot;
+    {  // Critical section begin
+      boost::lock_guard<boost::mutex> lock(i_mutex);
+      i_snapshot = --shared_i;
+    }  // Critical section end
 
-        // do some work with i_snapshot
-        // ...
-        (void) i_snapshot;
-    }
+    // do some work with i_snapshot
+    // ...
+    (void)i_snapshot;
+  }
 }
 
 void run() {
-    boost::thread t1(&do_inc);
-    boost::thread t2(&do_dec);
+  boost::thread t1(&do_inc);
+  boost::thread t2(&do_dec);
 
-    t1.join();
-    t2.join();
+  t1.join();
+  t2.join();
 
-    assert(shared_i == 0);
-    std::cout << "shared_i == " << shared_i;
+  assert(shared_i == 0);
+  std::cout << "shared_i == " << shared_i;
 }
 
-} // namespace without_sync
+}  // namespace with_sync
 
 #include <boost/thread/recursive_mutex.hpp>
 
 int main() {
-    without_sync::run();
-    std::cout << '\n';
-    with_sync::run();
-    std::cout << '\n';
+  without_sync::run();
+  std::cout << '\n';
+  with_sync::run();
+  std::cout << '\n';
 }
