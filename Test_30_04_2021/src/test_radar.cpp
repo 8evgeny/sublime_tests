76b394a30ebb31f6415cf59d3ac3e0056573cae7
Parubets Evgeny <parubets.e@alager.ru>
2021-05-01 10:23:49 +0300

    +

--- Test_30_04_2021/src/test_radar.cpp
+++ Test_30_04_2021/src/test_radar.cpp
@@ -20,19 +20,19 @@ int main(int argc, char** argv) {
   cout << "Minor version : " << CV_MINOR_VERSION << endl;
   cout << "Subminor version : " << CV_SUBMINOR_VERSION << endl;
 
-  Mat img, gray, edges;      // Объявление матриц
-  img = imread(argv[1], 1);  // Читаем изображение
-  imshow("original", img);   // Отрисовываем изображение
-  cvtColor(img, gray, COLOR_BGR2GRAY);
-  // Конвертируем в монохромный формат
-  GaussianBlur(gray, gray, Size(7, 7), 1.5);
-  // Устраняем размытие
-  Canny(gray, edges, 0, 50);
-  // Запускаем детектор ребер
-  imshow("edges", edges);
-  // Отрисовываем изображение
-  waitKey();
-  //Ожидаем нажатия клавиши
+  //  Mat img, gray, edges;      // Объявление матриц
+  //  img = imread(argv[1], 1);  // Читаем изображение
+  //  imshow("original", img);   // Отрисовываем изображение
+  //  cvtColor(img, gray, COLOR_BGR2GRAY);
+  //  // Конвертируем в монохромный формат
+  //  GaussianBlur(gray, gray, Size(7, 7), 1.5);
+  //  // Устраняем размытие
+  //  Canny(gray, edges, 0, 50);
+  //  // Запускаем детектор ребер
+  //  imshow("edges", edges);
+  //  // Отрисовываем изображение
+  //  waitKey();
+  //  //Ожидаем нажатия клавиши
 
   // create a new image which consists of
   // 3 channels
@@ -41,12 +41,12 @@ int main(int argc, char** argv) {
   // each pixels initialized to the value of (100, 250, 30) for Blue, Green and
   // Red planes respectively.
 
-  //  Mat image(600, 800, CV_8UC3, Scalar(100, 250, 30));
-  //  String windowName = "Window with Blank Image";  // Name of the window
-  //  namedWindow(windowName);                        // Create a window
-  //  imshow(windowName, image);  // Show our image inside the created window.
-  //  waitKey(0);                 // Wait for any keystroke in the window
-  //  destroyWindow(windowName);  // destroy the created window
+  Mat image(800, 1400, CV_8UC3, Scalar(0, 0, 30));
+  String windowName = "Window with Blank Image";  // Name of the window
+  namedWindow(windowName);                        // Create a window
+  imshow(windowName, image);  // Show our image inside the created window.
+  waitKey(0);                 // Wait for any keystroke in the window
+  destroyWindow(windowName);  // destroy the created window
 
   return 0;
 }
