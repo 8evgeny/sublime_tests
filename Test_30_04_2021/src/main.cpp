9cef3a380cb3d83cdd2bb2113b8be65e8ccbf66a
Evgeny P <8evgeny@gmail.com>
2021-05-05 21:40:44 +0300

    +

--- Test_30_04_2021/src/main.cpp
+++ Test_30_04_2021/src/main.cpp
@@ -89,6 +89,23 @@ bool checkValid(CoastalRadarMessage::Data msg, object::ToRadar obj)
 
 void movingObjects(Mat image, char* window_name)
 {
+    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+    // Or, for an ortho camera :
+    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+    // Camera matrix
+    glm::mat4 View = glm::lookAt(
+        glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+        glm::vec3(0, 0, 0), // and looks at the origin
+        glm::vec3(0, 1, 0) // Head is up (set to 0,-1,0 to look upside-down)
+    );
+
+    // Model matrix : an identity matrix (model will be at the origin)
+    glm::mat4 Model = glm::mat4(1.0f);
+
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
 #define green Scalar(0, 255, 0)
 #define axisX Scalar(255, 255, 100)
