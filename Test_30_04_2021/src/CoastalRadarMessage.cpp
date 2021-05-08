4f2987078e8170ed4b1491af3175385142b4207b
Evgeny P <8evgeny@gmail.com>
2021-05-08 18:54:41 +0300

    +

--- Test_30_04_2021/src/CoastalRadarMessage.cpp
+++ Test_30_04_2021/src/CoastalRadarMessage.cpp
@@ -1,5 +1,9 @@
 #include "CoastalRadarMessage.h"
 #include "SystemClock.h"
+
+CoastalRadarMessage::CoastalRadarMessage() { }
+CoastalRadarMessage::~CoastalRadarMessage() { }
+
 bool CoastalRadarMessage::is_data_valid() const
 {
     if (SystemClock::get_time_milliseconds() - data.timestamp < 100)
