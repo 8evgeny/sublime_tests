42bba056e6054b1ac8e6af0fadd852d9b12721f9
8evgeny <8evgeny@gmail.com>
2022-08-23 05:13:11 +0300

    27_1.1

--- 27_Разработка сетевых программ_Perl/1_1.pl
+++ 27_Разработка сетевых программ_Perl/1_1.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 use IO::File;
-my $file = shift;
-my $fh = IO::File->new($file);
-my $line = <$fh>;
+my $file = shift;              #Читаем параметр командной строки в переменную $file
+my $fh = IO::File->new($file); #Дескриптор присваивается переменной
+my $line = <$fh>;              #Чтение строки из дескриптора файла
 print $line;
