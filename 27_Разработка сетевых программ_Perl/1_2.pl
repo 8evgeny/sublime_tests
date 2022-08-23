5ca0f6bb3deb30f707dbcdc34a22bf8db662252b
Evgeny Parubets <8evgeny@gmail.com>
2022-08-23 15:17:13 +0300

    2_2

--- 27_Разработка сетевых программ_Perl/1_2.pl
+++ 27_Разработка сетевых программ_Perl/1_2.pl
@@ -2,7 +2,5 @@
 use IO::Socket;
 my $server = shift;                      #Читаем параметр командной строки в переменную $server
 my $fh = IO::Socket::INET->new($server); #Дескриптор присваивается переменной
-my $line = <$fh>;     
-my $line = <$fh>;  
-my $line = <$fh>;                     #Чтение строки из дескриптора сокета
+my $line = <$fh>;                        #Чтение строки из дескриптора сокета
 print $line;
