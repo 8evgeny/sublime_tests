9f3d6d8f4fa49cc690dd9caeac473ed89ba717b4
8evgeny <8evgeny@gmail.com>
2022-08-24 21:15:31 +0300

    +

--- 27_Разработка сетевых программ_Perl/2_3_pipe.pl
+++ 27_Разработка сетевых программ_Perl/2_3_pipe.pl
@@ -1,9 +1,42 @@
 #!/usr/bin/perl
  use strict;
- my $arg = shift || 10;
+ my $arg = shift || 200;
  pipe(READER,WRITER) or die "Can't open pipe: $!\n";
  if (fork ==0)
  {
     #Первый дочерний процесс выводит данные на устройство WRITER
     close READER;
+    select WRITER; $| = 1;
+    factorial($arg);
+    exit 0;
+ }
+ if (fork ==0)
+ {
+    #Второй дочерний процесс выводит данные на устройство WRITER
+    close READER;
+    select WRITER; $| = 1;
+    my $result = fibonacci($arg);
+    exit 0;
+ }
+ #Родительский процесс закрывает устройство WRITER и читает данные с устройства READER
+ close WRITER;
+ print while <READER>;
+ sub factorial 
+ {
+   my $target = shift;
+   for (my $result = 1, my $i = 1; $i<=$target; $i++)
+   {
+      print "factorial($i) => ",$result *= $i,"\n";
+   }
+ }
+ sub fibonacci
+ {
+   my $target = shift;
+   my ($a,$b) = (1,0);
+   for (my $i = 1; $i <= $target; $i++)
+   {
+      my $c = $a + $b;
+      print "fibonacci ($i) => $c\n";
+      ($a,$b) = ($b,$c);
+   }
  }
