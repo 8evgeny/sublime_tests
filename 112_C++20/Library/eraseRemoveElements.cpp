662e16785c7c187cde0b007051becdf7371ab2c3
8evgeny <8evgeny@gmail.com>
2022-11-12 21:04:28 +0300

    +++

--- 112_C++20/Library/eraseRemoveElements.cpp
+++ 112_C++20/Library/eraseRemoveElements.cpp
@@ -15,9 +15,9 @@ int main() {
 
     auto newEnd = std::remove_if(myVec.begin(), myVec.end(),        
                                  [](int ele){ return ele < 0; });
-    myVec.erase(newEnd, myVec.end());                              
-    // myVec.erase(std::remove_if(myVec.begin(), myVec.end(),       
-                   [](int ele){ return ele < 0; }), myVec.end());
+    myVec.erase(newEnd, myVec.end());
+//    myVec.erase(std::remove_if(myVec.begin(), myVec.end(),
+//                   [](int ele){ return ele < 0; }), myVec.end());
     for (auto ele: myVec) std::cout << ele << " ";
 
     std::cout << "\n\n";
