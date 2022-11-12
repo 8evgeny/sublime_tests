94c1ccb7259ef24f085898ccbecfb30d1271e2ae
8evgeny <8evgeny@gmail.com>
2022-11-12 16:15:38 +0300

    add

--- 112_C++20/Library/createSpan.cpp
+++ 112_C++20/Library/createSpan.cpp
@@ -4,17 +4,28 @@
 #include <iostream>
 #include <span>
 #include <vector>
-
+template<class T>
+void print(T cont){
+    for (auto &i:cont)
+        std::cout<<i<<" ";
+    std::cout<<"\n";
+}
 int main() {
 
     std::cout << '\n';
     std::cout << std::boolalpha;
 
-    std::vector myVec{1, 2, 3, 4, 5};
-	
-    std::span mySpan1{myVec};                                       
-    std::span mySpan2{myVec.data(), myVec.size()};                  
-	
+std::vector myVec{1, 2, 3, 4, 5};
+print(myVec);
+
+    std::span mySpan1{myVec};
+print(mySpan1);
+
+    std::span mySpan2{myVec.data(), myVec.size()};
+//    std::span mySpan2{myVec.data(), 3};
+print(mySpan2);
+
+
     bool spansEqual = std::equal(mySpan1.begin(), mySpan1.end(),
                                  mySpan2.begin(), mySpan2.end());
 	
