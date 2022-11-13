05de167a50dbbc8e51795a44ad656362e8664e31
8evgeny <8evgeny@gmail.com>
2022-11-13 09:50:55 +0300

    add format

--- 112_C++20/Library/formatArgumentID.cpp
+++ 112_C++20/Library/formatArgumentID.cpp
@@ -12,7 +12,7 @@ int main() {
 
     std::cout << fmt::format("{1} {0}: {2}!\n", "World", "Hello", 2020);
 
-    std::cout << fmt::format("{0} {0} {1}: {2}!\n", "Hello", "World", 2020);
+    std::cout << fmt::format("{0} {0} {1} {1}: {2}!\n", "Hello", "World", 2020);
 
     std::cout << fmt::format("{0}: {2}!\n", "Hello", "World", 2020);
     
