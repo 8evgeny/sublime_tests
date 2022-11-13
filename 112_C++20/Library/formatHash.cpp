18427c813ad941a2aaf64e34c75332d57ada283e
8evgeny <8evgeny@gmail.com>
2022-11-13 10:24:41 +0300

    ++

--- 112_C++20/Library/formatHash.cpp
+++ 112_C++20/Library/formatHash.cpp
@@ -6,8 +6,8 @@
 int main() {
 
     std::cout << fmt::format("{:+06d}", 120) << '\n';
-    std::cout << fmt::format("{:#0f}", 120) << '\n';
-    std::cout << fmt::format("{:0>15f}", 120) << '\n';
+//    std::cout << fmt::format("{:#0f}", 120) << '\n';
+//    std::cout << fmt::format("{:0>15f}", 120) << '\n';
     std::cout << fmt::format("{:#06x}", 0xa)  << '\n';
 
 }
