08e4aeeb922467098873a390641819530090c109
8evgeny <8evgeny@gmail.com>
2023-05-01 09:57:58 +0300

    ADD

--- SE/SE_v1/main.cpp
+++ SE/SE_v1/main.cpp
@@ -12,9 +12,9 @@ int main(int argc, char** argv)
         n = stoi(argv[1]);
     }
     nativ curr_Time(n);
-    nativ my("Evgeny");
+//    nativ my("Evgeny");
 //        nativ t("Танюша");
-//        nativ d1("Stasya");
+        nativ d1("Stasya");
     //    nativ d2("Taya");
     //    nativ d3("Maya");
     //    nativ d4("Ulyana");
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
     //  thread t1(&nativ::Qtvara, ref(curr_Time));
     //  thread t2(&nativ::Qtvara, ref(t));
 
-        curr_Time.scanHairCut(my, 60);
+        curr_Time.scanHairCut(d1, 360);
     //    curr_Time.scanHairCut(d2, 60);
     //    curr_Time.scanHairCut(d3, 60);
  //   curr_Time.scanHairCut(my, 60);
