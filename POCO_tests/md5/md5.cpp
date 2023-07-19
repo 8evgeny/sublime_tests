5007f1899ca5b7fccca24c94757d48dc9cba4dcd
Evgeny Parubets <8evgeny@gmail.com>
2023-07-19 16:24:13 +0300

    ++++

--- POCO_tests/md5/md5.cpp
+++ POCO_tests/md5/md5.cpp
@@ -6,7 +6,8 @@
  int main(int argc, char** argv){
      Poco::MD5Engine md5;
      Poco::DigestOutputStream ds(md5);
-     ds << "abcdefghijklmnopqrstuvwxyz";
+//     ds << "abcdefghijklmnopqrstuvwxyz";
+     ds << "123";
      ds.close();
      std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
      return 0;
