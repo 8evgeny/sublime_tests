9528d3376fa210e6d1b2212f733e4b0a08cb5c2e
8evgeny <8evgeny@gmail.com>
2023-09-16 12:09:52 +0300

    +

--- games/GAME C++/16_Games/02 Doodle Jump/main.cpp
+++ games/GAME C++/16_Games/02 Doodle Jump/main.cpp
@@ -9,7 +9,7 @@ int main()
 {
     srand(time(0));
 
-    RenderWindow app(VideoMode(400, 533), "Doodle Game!");
+    RenderWindow app(VideoMode(400, 533), "Hi Maya!");
     app.setFramerateLimit(60);
 
     Texture t1,t2,t3;
