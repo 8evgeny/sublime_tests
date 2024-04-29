88e749adb16be2516a88db557cd22a354850af5e
8evgeny <8evgeny@gmail.com>
2024-04-29 10:34:09 +0300

    FIX

--- 300_0penCV/3_showUI.cpp
+++ 300_0penCV/3_showUI.cpp
@@ -33,7 +33,7 @@ int main(int, char* argv[]) {
 // Создаем главное окно
     namedWindow(main_win);
 // Создаем шрифт для добавления текста поверх изображения
-    CvFont font = fontQt("Arial", 20, Scalar(255,0,0,0), CV_FONT_BLACK, CV_STYLE_NORMAL);
+    CvFont font = fontQt("Arial", 20, Scalar(255,0,0,0), CV_FONT_NORMAL, CV_STYLE_NORMAL);
 // Задаем функции обратного вызова
     setMouseCallback(main_win, cbMouse, NULL);
     createTrackbar(track1, main_win, &tb1_value, 100, tb1_Callback);
@@ -59,69 +59,68 @@ void cbMouse(int event, int x, int y, int flags, void*) {
     static bool p2set = false;
     // Left mouse button pressed
     if (event == EVENT_LBUTTONDOWN) {
-    p1 = Point(x, y); // Set orig. point
-    p2set = false;
-    } else if (event == EVENT_MOUSEMOVE &&
-    flags == EVENT_FLAG_LBUTTON) {
-    // Check moving mouse and left button down
-    // Check out bounds
-    if (x > orig_img.size().width)
-    x = orig_img.size().width;
-    else if (x < 0)
-    x = 0;
-    // Check out bounds
-    if (y > orig_img.size().height)
-    y = orig_img.size().height;
-    else if (y < 0)
-    y = 0;
-    p2 = Point(x, y); // Set final point
-    p2set = true;
-    // Copy orig. to temp. image
-    orig_img.copyTo(tmp_img);
-    // Draws rectangle
-    rectangle(tmp_img, p1, p2, Scalar(0, 0 ,255));
-    // Draw temporal image with rect.
-    imshow(main_win, tmp_img);
+        p1 = Point(x, y); // Set orig. point
+        p2set = false;
+    } else if (event == EVENT_MOUSEMOVE && flags == EVENT_FLAG_LBUTTON) {
+        // Check moving mouse and left button down
+        // Check out bounds
+        if (x > orig_img.size().width)
+        x = orig_img.size().width;
+        else if (x < 0)
+        x = 0;
+        // Check out bounds
+        if (y > orig_img.size().height)
+        y = orig_img.size().height;
+        else if (y < 0)
+        y = 0;
+        p2 = Point(x, y); // Set final point
+        p2set = true;
+        // Copy orig. to temp. image
+        orig_img.copyTo(tmp_img);
+        // Draws rectangle
+        rectangle(tmp_img, p1, p2, Scalar(0, 0 ,255));
+        // Draw temporal image with rect.
+        imshow(main_win, tmp_img);
     } else if (event == EVENT_LBUTTONUP && p2set) {
-    // Check if left button is released
-    // and selected an area
-    // Set subarray on orig. image
-    // with selected rectangle
-    Mat submat = orig_img(Rect(p1, p2));
-    // Here some processing for the submatrix
-    //...
-    // Mark the boundaries of selected rectangle
-    rectangle(orig_img, p1, p2, Scalar(0, 0, 255), 2);
-    imshow("main_win", orig_img);
+        // Check if left button is released
+        // and selected an area
+        // Set subarray on orig. image
+        // with selected rectangle
+        Mat submat = orig_img(Rect(p1, p2));
+        // Here some processing for the submatrix
+        //...
+        // Mark the boundaries of selected rectangle
+        rectangle(orig_img, p1, p2, Scalar(0, 0, 255), 2);
+        imshow("main_win", orig_img);
     }
     return;
  }
 void tb1_Callback(int value, void *) {
- sprintf(msg, "Trackbar 1 changed. New value=%d", value);
- displayOverlay(main_win, msg);
- return;
+    sprintf(msg, "Trackbar 1 changed. New value=%d", value);
+    displayOverlay(main_win, msg);
+    return;
 }
 void tb2_Callback(int value, void *) {
- sprintf(msg, "Trackbar 2 changed. New value=%d", value);
- displayStatusBar(main_win, msg, 1000);
- return;
+    sprintf(msg, "Trackbar 2 changed. New value=%d", value);
+    displayStatusBar(main_win, msg, 1000);
+    return;
 }
 void checkboxCallBack(int state, void *) {
- sprintf(msg, "Check box changed. New state=%d", state);
- displayStatusBar(main_win, msg);
- return;
+    sprintf(msg, "Check box changed. New state=%d", state);
+    displayStatusBar(main_win, msg);
+    return;
 }
 void radioboxCallBack(int state, void *id) {
- // Id of the radio box passed to the callBack
- sprintf(msg, "%s changed. New state=%d",
- (char *)id, state);
- displayStatusBar(main_win, msg);
- return;
+    // Id of the radio box passed to the callBack
+    sprintf(msg, "%s changed. New state=%d",
+    (char *)id, state);
+    displayStatusBar(main_win, msg);
+    return;
 }
 void pushbuttonCallBack(int, void *font) {
- // Add text to the image
- addText(orig_img, "Push button clicked",
- Point(50,50), *((CvFont *)font));
- imshow(main_win, orig_img); // Shows original image
- return;
+    // Add text to the image
+    addText(orig_img, "Push button clicked",
+    Point(50,50), *((CvFont *)font));
+    imshow(main_win, orig_img); // Shows original image
+    return;
 }
