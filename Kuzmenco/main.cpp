d546e278def53552c39de3fbd39ddaf5d10e4e8f
Evgeny <8evgeny@gmail.com>
2024-06-10 21:34:56 +0300

    fix

--- Kuzmenco/main.cpp
+++ Kuzmenco/main.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include <iostream>
 #include <QApplication>
 #include <curses.h>
@@ -7,6 +7,15 @@ using namespace std;
 char name[100];
 char date[100];
 int number,r_1,r_2,r_3,r_4,r_5,r_6,l_1,l_2,l_3,l_4,l_5,l_6;
+
+#define YELLOW attron(COLOR_PAIR(1));
+#define GREEN attron(COLOR_PAIR(2));
+#define WHITE attron(COLOR_PAIR(3));
+#define RED attron(COLOR_PAIR(4));
+#define BLUE attron(COLOR_PAIR(5));
+#define MAGENTA attron(COLOR_PAIR(6));
+#define CYAN attron(COLOR_PAIR(7));
+
 bool answer(uint num, string qw1, string qw2, string qw3, string qw4);
 //void colorMsg(uint num, vector<string> & qw);
 
@@ -18,20 +27,44 @@ bool answers(){
         return false;
     if (!answer(3, "","","","Введите номер тестирования  "))
         return false;
-    if (!answer(4, "P ","Канал легких, ", "Правая сторона. ", "Введите измеренное значение  "))
+    if (!answer(4, "P ","Легкие ", "Правая сторона ", "Введите измеренное значение  "))
         return false;
+    if (!answer(5, "MC ","Перикард ", "Правая сторона ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(6, "С ","Сердце ", "Правая сторона ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(7, "IG ","Тонкий кишечник ", "Правая сторона ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(8, "TR ","Гормональная система ", "Правая сторона ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(9, "GI ","Толстый кишечник ", "Правая сторона ", "Введите измеренное значение  "))
+        return false;
+
+    if (!answer(10, "P ","Легкие ", "Левая сторона ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(11, "MC ","Перикард ", "Левая сторона ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(12, "С ","Сердце ", "Левая сторона ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(13, "IG ","Тонкий кишечник ", "Левая сторона ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(14, "TR ","Гормональная система ", "Левая сторона ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(15, "GI ","Толстый кишечник ", "Левая сторона ", "Введите измеренное значение  "))
+        return false;
+    mvprintw(11, 0, "");
+
+  char tmp[10] ;
+  getstr(tmp);
+
+
 
      return true;
 }
 
 bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
 {
-    initscr();
-    start_color();
-    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
-    init_pair(2, COLOR_GREEN, COLOR_BLACK);
-    init_pair(3, COLOR_WHITE, COLOR_BLACK);
-    attron(COLOR_PAIR(3));
+    WHITE
 
     noecho();
     halfdelay(100);         //Устанавливаем ограничение по времени ожидания getch() в 10 сек
@@ -48,41 +81,279 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
     {
         echo();
         mvprintw(num, 0, qw4.c_str());
+        MAGENTA
         getstr(name);
-        mvprintw(num, 0, "                                               ");
-        mvprintw(num, 0, name);
+        YELLOW
+        mvprintw(num, 0, "Имя:          ");
+        mvprintw(num, 10, name);
+        clrtoeol();
     }
     if (num == 2)
     {
         echo();
         mvprintw(num, 0, qw4.c_str());
+        MAGENTA
         getstr(date);
-        mvprintw(num, 0, "                                               ");
-        mvprintw(num, 0, date);
+        YELLOW
+        mvprintw(num, 0, "Дата:           ");
+        mvprintw(num, 10, date);
+        clrtoeol();
     }
     if (num == 3)
     {
         echo();
         mvprintw(num, 0, qw4.c_str());
+        MAGENTA
         getstr(str);
-        mvprintw(num, 0, "                                               ");
+        YELLOW
+        mvprintw(num, 0, "Номер:            ");
         number = atoi(str);
-        mvprintw(num, 0, str);
+        mvprintw(num, 10, str);
+        clrtoeol();
     }
     if (num == 4)
     {
         auto start_line = num+1;
         echo();
         mvprintw(start_line, 0, qw1.c_str());
-        mvprintw(start_line, 5, qw2.c_str());
-        mvprintw(start_line, 22, qw3.c_str());
-        mvprintw(start_line, 42, qw4.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
         getstr(str);
         r_1 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 5)
+    {
+        auto start_line = num+1;
+        echo();
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_2 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 6)
+    {
+        auto start_line = num+1;
+        echo();
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_3 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 7)
+    {
+        auto start_line = num+1;
+        echo();
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_4 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 8)
+    {
+        auto start_line = num+1;
+        echo();
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_5 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 9)
+    {
+        auto start_line = num+1;
+        echo();
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_6 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 10)
+    {
+        echo();
+        mvprintw(11, 0, qw1.c_str());
+        mvprintw(11, 4, qw2.c_str());
+        mvprintw(11, 30, qw3.c_str());
+        mvprintw(11, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_1 = atoi(str);
+        RED
+        mvprintw(5, 35, str);
+    }
+    if (num == 11)
+    {
+        echo();
+        mvprintw(11, 0, qw1.c_str());
+        mvprintw(11, 4, qw2.c_str());
+        mvprintw(11, 30, qw3.c_str());
+        mvprintw(11, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_2 = atoi(str);
+        RED
+        mvprintw(6, 35, str);
+    }
+    if (num == 12)
+    {
+        echo();
+        mvprintw(11, 0, qw1.c_str());
+        mvprintw(11, 4, qw2.c_str());
+        mvprintw(11, 30, qw3.c_str());
+        mvprintw(11, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_3 = atoi(str);
+        RED
+        mvprintw(7, 35, str);
+    }
+    if (num == 13)
+    {
+        echo();
+        mvprintw(11, 0, qw1.c_str());
+        mvprintw(11, 4, qw2.c_str());
+        mvprintw(11, 30, qw3.c_str());
+        mvprintw(11, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_4 = atoi(str);
+        RED
+        mvprintw(8, 35, str);
+    }
+    if (num == 14)
+    {
+        echo();
+        mvprintw(11, 0, qw1.c_str());
+        mvprintw(11, 4, qw2.c_str());
+        mvprintw(11, 30, qw3.c_str());
+        mvprintw(11, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_5 = atoi(str);
+        RED
+        mvprintw(9, 35, str);
+    }
+    if (num == 15)
+    {
+        echo();
+        mvprintw(11, 0, qw1.c_str());
+        mvprintw(11, 4, qw2.c_str());
+        mvprintw(11, 30, qw3.c_str());
+        mvprintw(11, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_6 = atoi(str);
+        RED
+        mvprintw(10, 35, str);
     }
 
 
 
+
+
+    refresh(); //Выводим на настоящий экран
+    endwin();
+
+ return ret;
+}
+
+//void colorMsg(uint num, vector<string> & qw)
+//{
+//    mvprintw(num*2-2, 0, qw[0].c_str());
+//    attron(COLOR_PAIR(1));
+//    printw(qw[1].c_str());
+//    attron(COLOR_PAIR(3));
+//    printw(qw[2].c_str());
+//    attron(COLOR_PAIR(2));
+//    printw(qw[3].c_str());
+//    attron(COLOR_PAIR(3));
+//    printw(qw[4].c_str());
+//}
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+    initscr();
+    start_color();
+    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);
+    init_pair(4, COLOR_RED, COLOR_BLACK);
+    init_pair(5, COLOR_BLUE, COLOR_BLACK);
+    init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(7, COLOR_CYAN, COLOR_BLACK);
+    bkgdset(COLOR_PAIR(2));
+    if (!answers()){
+        cout << "\nИсправьте ИУЛ !!!\n" <<endl;
+        return 0;
+    }
+    cout<<"Имя: "<<name<<endl;
+    cout<<"Дата: "<<date<<endl;
+    cout<<"Номер: "<<number<<endl;
+    cout<< "Ответы на вопросы ... OK" <<endl;
+
+    return a.exec();
+}
 //    while ( !ex ) {
 //        int rc = get_wch(&tmp);
 //        if (rc==OK) {
@@ -142,42 +413,3 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
 //            break;
 //        }
 //    }
-
-
-    refresh(); //Выводим на настоящий экран
-    endwin();
-
- return ret;
-}
-
-//void colorMsg(uint num, vector<string> & qw)
-//{
-//    mvprintw(num*2-2, 0, qw[0].c_str());
-//    attron(COLOR_PAIR(1));
-//    printw(qw[1].c_str());
-//    attron(COLOR_PAIR(3));
-//    printw(qw[2].c_str());
-//    attron(COLOR_PAIR(2));
-//    printw(qw[3].c_str());
-//    attron(COLOR_PAIR(3));
-//    printw(qw[4].c_str());
-//}
-
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
-
-    if (!answers()){
-        cout << "\nИсправьте ИУЛ !!!\n" <<endl;
-        return 0;
-    }
-    cout<<"Имя: "<<name<<endl;
-    cout<<"Дата: "<<date<<endl;
-    cout<<"Номер: "<<number<<endl;
-    cout<< "Ответы на вопросы ... OK" <<endl;
-
-    return a.exec();
-}
