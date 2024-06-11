1c88bfd3b28b6b68651db3dbfce0022007150e04
Evgeny <8evgeny@gmail.com>
2024-06-11 07:26:04 +0300

    fix

--- Kuzmenco/main.cpp
+++ Kuzmenco/main.cpp
@@ -3,11 +3,15 @@
 #include <QApplication>
 #include <curses.h>
 #include <ncursesw/ncurses.h>
+#include <math.h>
+
 using namespace std;
 char name[100];
 char date[100];
-int number,r_1,r_2,r_3,r_4,r_5,r_6,l_1,l_2,l_3,l_4,l_5,l_6;
-
+int number{0}, summ{0};
+int r_1{0}, r_2{0}, r_3{0}, r_4{0}, r_5{0}, r_6{0}, l_1{0}, l_2{0}, l_3{0}, l_4{0}, l_5{0}, l_6{0};
+int r_7{0}, r_8{0}, r_9{0}, r_10{0}, r_11{0}, r_12{0}, l_7{0}, l_8{0}, l_9{0}, l_10{0}, l_11{0},l_12{0};
+float average{0.0};
 #define YELLOW attron(COLOR_PAIR(1));
 #define GREEN attron(COLOR_PAIR(2));
 #define WHITE attron(COLOR_PAIR(3));
@@ -27,36 +31,70 @@ bool answers(){
         return false;
     if (!answer(3, "","","","Введите номер тестирования  "))
         return false;
-    if (!answer(4, "P ","Легкие ", "Правая сторона ", "Введите измеренное значение  "))
+    if (!answer(4, "P ","Легкие ", "Правая рука ", "Введите измеренное значение  "))
         return false;
-    if (!answer(5, "MC ","Перикард ", "Правая сторона ", "Введите измеренное значение  "))
+    if (!answer(5, "MC ","Перикард ", "Правая рука ", "Введите измеренное значение  "))
         return false;
-    if (!answer(6, "С ","Сердце ", "Правая сторона ", "Введите измеренное значение  "))
+    if (!answer(6, "С ","Сердце ", "Правая рука ", "Введите измеренное значение  "))
         return false;
-    if (!answer(7, "IG ","Тонкий кишечник ", "Правая сторона ", "Введите измеренное значение  "))
+    if (!answer(7, "IG ","Тонкий кишечник ", "Правая рука ", "Введите измеренное значение  "))
         return false;
-    if (!answer(8, "TR ","Гормональная система ", "Правая сторона ", "Введите измеренное значение  "))
+    if (!answer(8, "TR ","Гормональная система ", "Правая рука ", "Введите измеренное значение  "))
         return false;
-    if (!answer(9, "GI ","Толстый кишечник ", "Правая сторона ", "Введите измеренное значение  "))
+    if (!answer(9, "GI ","Толстый кишечник ", "Правая рука ", "Введите измеренное значение  "))
         return false;
 
-    if (!answer(10, "P ","Легкие ", "Левая сторона ", "Введите измеренное значение  "))
+    if (!answer(10, "P ","Легкие ", "Левая рука ", "Введите измеренное значение  "))
         return false;
-    if (!answer(11, "MC ","Перикард ", "Левая сторона ", "Введите измеренное значение  "))
+    if (!answer(11, "MC ","Перикард ", "Левая рука ", "Введите измеренное значение  "))
         return false;
-    if (!answer(12, "С ","Сердце ", "Левая сторона ", "Введите измеренное значение  "))
+    if (!answer(12, "С ","Сердце ", "Левая рука ", "Введите измеренное значение  "))
         return false;
-    if (!answer(13, "IG ","Тонкий кишечник ", "Левая сторона ", "Введите измеренное значение  "))
+    if (!answer(13, "IG ","Тонкий кишечник ", "Левая рука ", "Введите измеренное значение  "))
         return false;
-    if (!answer(14, "TR ","Гормональная система ", "Левая сторона ", "Введите измеренное значение  "))
+    if (!answer(14, "TR ","Гормональная система ", "Левая рука ", "Введите измеренное значение  "))
         return false;
-    if (!answer(15, "GI ","Толстый кишечник ", "Левая сторона ", "Введите измеренное значение  "))
+    if (!answer(15, "GI ","Толстый кишечник ", "Левая рука ", "Введите измеренное значение  "))
         return false;
-    mvprintw(11, 0, "");
 
+    if (!answer(16, "P ","Легкие ", "Правая нога ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(17, "MC ","Перикард ", "Правая нога ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(18, "С ","Сердце ", "Правая нога ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(19, "IG ","Тонкий кишечник ", "Правая нога ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(20, "TR ","Гормональная система ", "Правая нога ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(21, "GI ","Толстый кишечник ", "Правая нога ", "Введите измеренное значение  "))
+        return false;
+
+    if (!answer(22, "P ","Легкие ", "Левая нога ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(23, "MC ","Перикард ", "Левая нога ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(24, "С ","Сердце ", "Левая нога ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(25, "IG ","Тонкий кишечник ", "Левая нога ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(26, "TR ","Гормональная система ", "Левая нога ", "Введите измеренное значение  "))
+        return false;
+    if (!answer(27, "GI ","Толстый кишечник ", "Левая нога ", "Введите измеренное значение  "))
+        return false;
+
+    WHITE
+    mvprintw(18, 0, "");
+    summ = l_1+l_2+l_3+l_4+l_5+l_6+l_7+l_8+l_9+l_10+l_11+l_12+
+            r_1+r_2+r_3+r_4+r_5+r_6+r_7+r_8+r_9+r_10+r_11+r_12;
+    average = floorf((float)summ/24 * 10) / 10;
+
+    mvprintw(19, 0, "Коридор нормы верх");
+    mvprintw(19, 30, "%4.1f", average + 7);
+    mvprintw(20, 0, "Коридор нормы низ");
+    mvprintw(20, 30, "%4.1f", average - 7);
   char tmp[10] ;
   getstr(tmp);
-
 
 
      return true;
@@ -65,8 +103,8 @@ bool answers(){
 bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
 {
     WHITE
+    echo();
 
-    noecho();
     halfdelay(100);         //Устанавливаем ограничение по времени ожидания getch() в 10 сек
     bool ret = true;
     bool ex = false;
@@ -79,7 +117,6 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
     char str[100];
     if (num == 1)
     {
-        echo();
         mvprintw(num, 0, qw4.c_str());
         MAGENTA
         getstr(name);
@@ -90,7 +127,6 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
     }
     if (num == 2)
     {
-        echo();
         mvprintw(num, 0, qw4.c_str());
         MAGENTA
         getstr(date);
@@ -101,7 +137,6 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
     }
     if (num == 3)
     {
-        echo();
         mvprintw(num, 0, qw4.c_str());
         MAGENTA
         getstr(str);
@@ -111,10 +146,10 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
         mvprintw(num, 10, str);
         clrtoeol();
     }
+    //Правая рука
     if (num == 4)
     {
         auto start_line = num+1;
-        echo();
         mvprintw(start_line, 0, qw1.c_str());
         mvprintw(start_line, 4, qw2.c_str());
         mvprintw(start_line, 30, qw3.c_str());
@@ -132,7 +167,6 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
     if (num == 5)
     {
         auto start_line = num+1;
-        echo();
         mvprintw(start_line, 0, qw1.c_str());
         mvprintw(start_line, 4, qw2.c_str());
         mvprintw(start_line, 30, qw3.c_str());
@@ -150,7 +184,6 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
     if (num == 6)
     {
         auto start_line = num+1;
-        echo();
         mvprintw(start_line, 0, qw1.c_str());
         mvprintw(start_line, 4, qw2.c_str());
         mvprintw(start_line, 30, qw3.c_str());
@@ -168,7 +201,6 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
     if (num == 7)
     {
         auto start_line = num+1;
-        echo();
         mvprintw(start_line, 0, qw1.c_str());
         mvprintw(start_line, 4, qw2.c_str());
         mvprintw(start_line, 30, qw3.c_str());
@@ -186,7 +218,6 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
     if (num == 8)
     {
         auto start_line = num+1;
-        echo();
         mvprintw(start_line, 0, qw1.c_str());
         mvprintw(start_line, 4, qw2.c_str());
         mvprintw(start_line, 30, qw3.c_str());
@@ -204,7 +235,6 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
     if (num == 9)
     {
         auto start_line = num+1;
-        echo();
         mvprintw(start_line, 0, qw1.c_str());
         mvprintw(start_line, 4, qw2.c_str());
         mvprintw(start_line, 30, qw3.c_str());
@@ -219,9 +249,9 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
         clrtoeol();
         mvprintw(start_line, 30, str);
     }
+    //Левая рука
     if (num == 10)
     {
-        echo();
         mvprintw(11, 0, qw1.c_str());
         mvprintw(11, 4, qw2.c_str());
         mvprintw(11, 30, qw3.c_str());
@@ -231,11 +261,10 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
         getstr(str);
         l_1 = atoi(str);
         RED
-        mvprintw(5, 35, str);
+        mvprintw(num - 5, 35, str);
     }
     if (num == 11)
     {
-        echo();
         mvprintw(11, 0, qw1.c_str());
         mvprintw(11, 4, qw2.c_str());
         mvprintw(11, 30, qw3.c_str());
@@ -245,11 +274,10 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
         getstr(str);
         l_2 = atoi(str);
         RED
-        mvprintw(6, 35, str);
+        mvprintw(num - 5, 35, str);
     }
     if (num == 12)
     {
-        echo();
         mvprintw(11, 0, qw1.c_str());
         mvprintw(11, 4, qw2.c_str());
         mvprintw(11, 30, qw3.c_str());
@@ -259,11 +287,10 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
         getstr(str);
         l_3 = atoi(str);
         RED
-        mvprintw(7, 35, str);
+        mvprintw(num - 5, 35, str);
     }
     if (num == 13)
     {
-        echo();
         mvprintw(11, 0, qw1.c_str());
         mvprintw(11, 4, qw2.c_str());
         mvprintw(11, 30, qw3.c_str());
@@ -273,11 +300,10 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
         getstr(str);
         l_4 = atoi(str);
         RED
-        mvprintw(8, 35, str);
+        mvprintw(num - 5, 35, str);
     }
     if (num == 14)
     {
-        echo();
         mvprintw(11, 0, qw1.c_str());
         mvprintw(11, 4, qw2.c_str());
         mvprintw(11, 30, qw3.c_str());
@@ -287,11 +313,10 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
         getstr(str);
         l_5 = atoi(str);
         RED
-        mvprintw(9, 35, str);
+        mvprintw(num - 5, 35, str);
     }
     if (num == 15)
     {
-        echo();
         mvprintw(11, 0, qw1.c_str());
         mvprintw(11, 4, qw2.c_str());
         mvprintw(11, 30, qw3.c_str());
@@ -301,11 +326,194 @@ bool answer(uint num, string qw1, string qw2, string qw3, string qw4)
         getstr(str);
         l_6 = atoi(str);
         RED
-        mvprintw(10, 35, str);
+        mvprintw(num - 5, 35, str);
     }
 
+    mvprintw(11, 0, " ");clrtoeol();
 
-
+    //Правая нога
+    if (num == 16)
+    {
+        auto start_line = num - 4;
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_7 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 17)
+    {
+        auto start_line = num - 4;
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_8 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 18)
+    {
+        auto start_line = num - 4;
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_9 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 19)
+    {
+        auto start_line = num - 4;
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_10 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 20)
+    {
+        auto start_line = num - 4;
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_11 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    if (num == 21)
+    {
+        auto start_line = num - 4;
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        mvprintw(start_line, 30, qw3.c_str());
+        mvprintw(start_line, 48, qw4.c_str());
+        MAGENTA
+        getstr(str);
+        r_12 = atoi(str);
+        YELLOW
+        mvprintw(start_line, 0, qw1.c_str());
+        mvprintw(start_line, 4, qw2.c_str());
+        RED
+        clrtoeol();
+        mvprintw(start_line, 30, str);
+    }
+    //Левая нога
+    if (num == 22)
+    {
+        mvprintw(18, 0, qw1.c_str());
+        mvprintw(18, 4, qw2.c_str());
+        mvprintw(18, 30, qw3.c_str());
+        mvprintw(18, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_7 = atoi(str);
+        RED
+        mvprintw(num - 10, 35, str);
+    }
+    if (num == 23)
+    {
+        mvprintw(18, 0, qw1.c_str());
+        mvprintw(18, 4, qw2.c_str());
+        mvprintw(18, 30, qw3.c_str());
+        mvprintw(18, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_8 = atoi(str);
+        RED
+        mvprintw(num - 10, 35, str);
+    }
+    if (num == 24)
+    {
+        mvprintw(18, 0, qw1.c_str());
+        mvprintw(18, 4, qw2.c_str());
+        mvprintw(18, 30, qw3.c_str());
+        mvprintw(18, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_9 = atoi(str);
+        RED
+        mvprintw(num - 10, 35, str);
+    }
+    if (num == 25)
+    {
+        mvprintw(18, 0, qw1.c_str());
+        mvprintw(18, 4, qw2.c_str());
+        mvprintw(18, 30, qw3.c_str());
+        mvprintw(18, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_10 = atoi(str);
+        RED
+        mvprintw(num - 10, 35, str);
+    }
+    if (num == 26)
+    {
+        mvprintw(18, 0, qw1.c_str());
+        mvprintw(18, 4, qw2.c_str());
+        mvprintw(18, 30, qw3.c_str());
+        mvprintw(18, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_11 = atoi(str);
+        RED
+        mvprintw(num - 10, 35, str);
+    }
+    if (num == 27)
+    {
+        mvprintw(18, 0, qw1.c_str());
+        mvprintw(18, 4, qw2.c_str());
+        mvprintw(18, 30, qw3.c_str());
+        mvprintw(18, 48, qw4.c_str());
+        MAGENTA
+        clrtoeol();
+        getstr(str);
+        l_12 = atoi(str);
+        RED
+        mvprintw(num - 10, 35, str);
+    }
+    mvprintw(18, 0, " ");clrtoeol();
 
 
     refresh(); //Выводим на настоящий экран
@@ -350,7 +558,7 @@ int main(int argc, char *argv[])
     cout<<"Имя: "<<name<<endl;
     cout<<"Дата: "<<date<<endl;
     cout<<"Номер: "<<number<<endl;
-    cout<< "Ответы на вопросы ... OK" <<endl;
+    cout<< "Среднее" <<average<<endl;
 
     return a.exec();
 }
