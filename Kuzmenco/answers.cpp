a389775b4f31712481a26cce41a52af257ee1e31
Evgeny <8evgeny@gmail.com>
2024-06-20 23:56:15 +0300

    fix

--- Kuzmenco/answers.cpp
+++ Kuzmenco/answers.cpp
@@ -12,14 +12,26 @@
 
 using namespace std;
 
-extern char name[100];
-extern char date[100];
-extern int number, summ;
-extern int r_1, r_2, r_3, r_4, r_5, r_6, l_1, l_2, l_3, l_4, l_5, l_6;
-extern int r_7, r_8, r_9, r_10, r_11, r_12, l_7, l_8, l_9, l_10, l_11, l_12;
-extern float k1, k2 ,k3, k4, k5, k6, k7, k8, k9, k10, k11, k12;
-extern float average, hight, low;
-
+char name[100];
+char date[100];
+int number{0}, summ{0};
+int r_1{0}, r_2{0}, r_3{0}, r_4{0}, r_5{0}, r_6{0}, l_1{0}, l_2{0}, l_3{0}, l_4{0}, l_5{0}, l_6{0};
+int r_7{0}, r_8{0}, r_9{0}, r_10{0}, r_11{0}, r_12{0}, l_7{0}, l_8{0}, l_9{0}, l_10{0}, l_11{0},l_12{0};
+float k1 = 90.f/105;
+float k2 = 130.f/140.f;
+float k3 = 100.f/90;
+float k4 = 105.f/110;
+float k5 = 110.f/135.f;
+float k6 = 110.f/135.f;
+float k7 = 1;
+float k8 = 75.f/60;
+float k9 = 1;
+float k10 = 110.f/100;
+float k11 = 75.f/60;
+float k12 = 90.f/80;
+float average{0.0};
+float hight{0.0};
+float low{0.0};
 
 void main_logic()
 {
--- Kuzmenco/main.cpp
+++ Kuzmenco/main.cpp
@@ -7,30 +7,11 @@
 #include <ncursesw/ncurses.h>
 #include <math.h>
 #include <QLabel>
+#include <QStyle>
+#include <QDesktopWidget>
 
 using namespace std::chrono;
 using namespace std;
-
-char name[100];
-char date[100];
-int number{0}, summ{0};
-int r_1{0}, r_2{0}, r_3{0}, r_4{0}, r_5{0}, r_6{0}, l_1{0}, l_2{0}, l_3{0}, l_4{0}, l_5{0}, l_6{0};
-int r_7{0}, r_8{0}, r_9{0}, r_10{0}, r_11{0}, r_12{0}, l_7{0}, l_8{0}, l_9{0}, l_10{0}, l_11{0},l_12{0};
-float k1 = 90.f/105;
-float k2 = 130.f/140.f;
-float k3 = 100.f/90;
-float k4 = 105.f/110;
-float k5 = 110.f/135.f;
-float k6 = 110.f/135.f;
-float k7 = 1;
-float k8 = 75.f/60;
-float k9 = 1;
-float k10 = 110.f/100;
-float k11 = 75.f/60;
-float k12 = 90.f/80;
-float average{0.0};
-float hight{0.0};
-float low{0.0};
 
 
 int main(int argc, char *argv[])
@@ -38,26 +19,29 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow * w = new MainWindow;
     w->getUi()->centralwidget->setFixedSize(1850,1000);
-//    w.getUi()->centralwidget->
+
+    QDesktopWidget *desktop = QApplication::desktop();
+
+    int screenWidth = desktop->width();
+    int screenHeight = desktop->height();
+    int x = (screenWidth - w->width()) / 2;
+    int y = (screenHeight - w->height()) / 2;
+    w->move(x, y);
+
 
     QPixmap      pix("/home/user/SOFT/Github/Sublime_tests/Kuzmenco/table.png");
 
     QPalette pal;
     pal.setBrush(w->backgroundRole(), QBrush(pix));
+    w->getUi()->centralwidget->setGeometry(0,0,1920,1080);
+
     w->getUi()->frame->setPalette(pal);
     w->getUi()->frame->setAutoFillBackground(true);
     w->getUi()->frame->setFixedSize(pix.width(), pix.height());
-//    w->getUi()->centralwidget->setWindowFlags(Qt::Window | Qt::CustomizeWindowHint);
-
-
 
 
 
 //    main_logic();
-
-
-
-
 
     w->show();
     a.exec();
