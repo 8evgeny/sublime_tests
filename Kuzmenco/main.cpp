1309bae5cc360f96065c2ffa5e0b9b82297550b1
Evgeny <8evgeny@gmail.com>
2024-06-22 19:56:05 +0300

    fix

--- Kuzmenco/main.cpp
+++ Kuzmenco/main.cpp
@@ -18,26 +18,23 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow * w = new MainWindow;
-    w->getUi()->centralwidget->setFixedSize(1850,1000);
+//    w->getUi()->centralwidget->setFixedSize(1920,1080);
 
-    QDesktopWidget *desktop = QApplication::desktop();
-
+//    QDesktopWidget *desktop = QApplication::desktop();
 //    QRect screenres = QApplication::desktop()->screenGeometry(1/*screenNumber*/);
+//    int screenWidth = desktop->width();
+//    int screenHeight = desktop->height();
+//    int x = (screenWidth - w->width()) / 2;
+//    int y = (screenHeight - w->height()) / 2;
+//    w->move(x, y);
 
 
-    int screenWidth = desktop->width();
-    int screenHeight = desktop->height();
-    int x = (screenWidth - w->width()) / 2;
-    int y = (screenHeight - w->height()) / 2;
-    w->move(x, y);
-
-
-    QPixmap      pix("/home/user/SOFT/Github/Sublime_tests/Kuzmenco/table.png");
-
+    QPixmap      pix("../table.png");
     QPalette pal;
     pal.setBrush(w->backgroundRole(), QBrush(pix));
-//    w->getUi()->centralwidget->setGeometry(0,0,1920,1080);
-
+    QRect rect(20,20,1800,1080);
+    w->getUi()->horizontalLayout->setGeometry(rect);
+//w->getUi()->horizontalLayout->setAlignment(w->getUi()->widget,Qt::Alignment(Qt::LeftToRight));
     w->getUi()->widget->setPalette(pal);
     w->getUi()->widget->setAutoFillBackground(true);
     w->getUi()->widget->setFixedSize(pix.width(), pix.height() );
