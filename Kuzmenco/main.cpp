3f725979485b49faeec4379feb3ac0a4ccd8102c
Evgeny <8evgeny@gmail.com>
2024-06-28 22:56:28 +0300

    ++

--- Kuzmenco/main.cpp
+++ Kuzmenco/main.cpp
@@ -25,16 +25,6 @@ int main(int argc, char *argv[])
 {
     app = new QApplication(argc, argv);
     MainWindow * w = new MainWindow;
-//    w->getUi()->centralwidget->setFixedSize(1920,1080);
-
-//    QDesktopWidget *desktop = QApplication::desktop();
-//    QRect screenres = QApplication::desktop()->screenGeometry(1/*screenNumber*/);
-//    int screenWidth = desktop->width();
-//    int screenHeight = desktop->height();
-//    int x = (screenWidth - w->width()) / 2;
-//    int y = (screenHeight - w->height()) / 2;
-//    w->move(x, y);
-
 
     QPixmap      pix("table.png");
     QPalette pal;
