c2eb70e3f18a79b4c6a21d2247fe19a77a4a0f7d
Evgeny <8evgeny@gmail.com>
2024-07-01 17:24:00 +0300

    FIX

--- Current/openCL_project_1_min_max/find_min_max/workSerialMatches.cpp
+++ Current/openCL_project_1_min_max/find_min_max/workSerialMatches.cpp
@@ -1,6 +1,12 @@
 #include <QtCore>
 #include <QImage>
 #include "main.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+using namespace std;
+using namespace cv;
 
 extern QElapsedTimer eTimer;
 extern quint64 timeSerial;
@@ -11,13 +17,11 @@ void workSerialMatches(QImage &imageIn, QImage &imageTempl, QImage &imageOut)
     imageIn.save("matches-in.png");
     imageTempl.save("matches-templ.png");
     eTimer.restart();
-
-
-
-
-
-
-
+    Mat matIn(imageIn.height(), imageIn.width(), CV_8UC3, (cv::Scalar*)imageIn.scanLine(0));
+    Mat matTempl(imageTempl.height(), imageTempl.width(), CV_8UC3, (cv::Scalar*)imageTempl.scanLine(0));
+    Mat matOut(imageOut.height(), imageOut.width(), CV_8UC3, (cv::Scalar*)imageOut.scanLine(0));
+    matchTemplate( matIn, matTempl, matOut, TM_SQDIFF);
+    imageOut = QImage((uchar*) matOut.data, /*matOut.cols*/640, /*matOut.rows*/480, matOut.step, QImage::Format_Grayscale8/*Format_RGB888*/);
     timeSerial = eTimer.nsecsElapsed();
     imageOut.save("matches-serial.png");
 }
