eba537c50dd47bdf3ec7e62f76f7383acd688468
Evgeny <8evgeny@gmail.com>
2024-07-01 12:52:32 +0300

    ++

--- Current/openCL_project_1_min_max/find_min_max/find_min_max.cpp
+++ Current/openCL_project_1_min_max/find_min_max/find_min_max.cpp
@@ -3,6 +3,8 @@
 #include <CL/cl.hpp>
 
 #define WORKGROUPSIZE 16
+QElapsedTimer eTimer;
+quint64 timeSerial, timeParallel;
 
 inline void calculateNDRange(int size, int groupSize,
                              int *bSize, int *bGroupSize, int *oSize)
@@ -18,6 +20,73 @@ inline void calculateNDRange(int size, int groupSize,
     *bSize = size;
     *bGroupSize = groupSize;
     *oSize = size / groupSize;
+}
+
+void workSerial(QImage &imageIn, QImage &imageOut)
+{
+    eTimer.start();
+
+    // Serial edge detector
+    for (int y = 0; y < imageIn.height(); y++) {
+        QRgb *iLine = (QRgb *) imageIn.scanLine(y);
+
+        QRgb *iLine_m1 = (y < 1)?
+                             iLine:
+                             (QRgb *) imageIn.scanLine(y - 1);
+
+        QRgb *iLine_p1 = (y >= imageIn.height())?
+                             iLine:
+                             (QRgb *) imageIn.scanLine(y + 1);
+
+        QRgb *oLine = (QRgb *) imageOut.scanLine(y);
+
+        for (int x = 0; x < imageIn.width(); x++)
+        {
+            int x_m = (x < 1)? x: x - 1;
+            int x_p = (x >= imageIn.width())? x: x + 1;
+
+            // Sobel operator
+            int grayX = qRed(iLine_p1[x_m])
+                      + 2 * qRed(iLine_p1[x])
+                      + qRed(iLine_p1[x_p])
+                      - qRed(iLine_m1[x_m])
+                      - 2 * qRed(iLine_m1[x])
+                      - qRed(iLine_m1[x_p]);
+
+            int grayY = qRed(iLine_m1[x_p])
+                      + 2 * qRed(iLine[x_p])
+                      + qRed(iLine_p1[x_p])
+                      - qRed(iLine_m1[x_m])
+                      - 2 * qRed(iLine[x_m])
+                      - qRed(iLine_p1[x_m]);
+
+            int gray = sqrt(grayX * grayX + grayY * grayY);
+            gray = qBound(0, gray, 255);
+
+            oLine[x] = qRgb(gray, gray, gray);
+        }
+    }
+    timeSerial = eTimer.nsecsElapsed();
+    qDebug() << "serial   " << (float)timeSerial/1000000 << " ms";
+
+    imageOut.save("edges-serial.jpg");
+}
+
+void convertToGrey(QImage &imageIn)
+{
+    // Convert to gray scale
+    for (int y = 0; y < imageIn.height(); y++)
+    {
+        QRgb *line = (QRgb *) imageIn.scanLine(y);
+
+        for (int x = 0; x < imageIn.width(); x++)
+        {
+            int r = qGray(line[x]);
+            int g = r;
+            int b = r;
+            line[x] = qRgb(r, g, b);
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -73,70 +142,10 @@ int main(int argc, char *argv[])
 
     QImage imageIn("image.jpg");
     imageIn = imageIn.convertToFormat(QImage::Format_ARGB32);
-
-    // Convert to gray scale
-    for (int y = 0; y < imageIn.height(); y++)
-    {
-        QRgb *line = (QRgb *) imageIn.scanLine(y);
-
-        for (int x = 0; x < imageIn.width(); x++)
-        {
-            int r = qGray(line[x]);
-            int g = r;
-            int b = r;
-            line[x] = qRgb(r, g, b);
-        }
-    }
-
+    convertToGrey(imageIn);
     QImage imageOut(imageIn.size(), imageIn.format());
 
-    QElapsedTimer eTimer;
-    eTimer.start();
-
-    // Serial edge detector
-    for (int y = 0; y < imageIn.height(); y++) {
-        QRgb *iLine = (QRgb *) imageIn.scanLine(y);
-
-        QRgb *iLine_m1 = (y < 1)?
-                             iLine:
-                             (QRgb *) imageIn.scanLine(y - 1);
-
-        QRgb *iLine_p1 = (y >= imageIn.height())?
-                             iLine:
-                             (QRgb *) imageIn.scanLine(y + 1);
-
-        QRgb *oLine = (QRgb *) imageOut.scanLine(y);
-
-        for (int x = 0; x < imageIn.width(); x++)
-        {
-            int x_m = (x < 1)? x: x - 1;
-            int x_p = (x >= imageIn.width())? x: x + 1;
-
-            // Sobel operator
-            int grayX = qRed(iLine_p1[x_m])
-                      + 2 * qRed(iLine_p1[x])
-                      + qRed(iLine_p1[x_p])
-                      - qRed(iLine_m1[x_m])
-                      - 2 * qRed(iLine_m1[x])
-                      - qRed(iLine_m1[x_p]);
-
-            int grayY = qRed(iLine_m1[x_p])
-                      + 2 * qRed(iLine[x_p])
-                      + qRed(iLine_p1[x_p])
-                      - qRed(iLine_m1[x_m])
-                      - 2 * qRed(iLine[x_m])
-                      - qRed(iLine_p1[x_m]);
-
-            int gray = sqrt(grayX * grayX + grayY * grayY);
-            gray = qBound(0, gray, 255);
-
-            oLine[x] = qRgb(gray, gray, gray);
-        }
-    }
-    auto timeSerial = eTimer.nsecsElapsed();
-    qDebug() << "serial   " << (float)timeSerial/1000000 << " ms";
-
-    imageOut.save("edges-serial.jpg");
+    workSerial(imageIn, imageOut);
 
 
 
@@ -197,7 +206,7 @@ int main(int argc, char *argv[])
     // Retrieve output image
     commandQueue.enqueueReadImage(clImageOut, CL_TRUE, origin, region, 0, 0, imageOut.bits());
 
-    auto timeParallel = eTimer.nsecsElapsed();
+    timeParallel = eTimer.nsecsElapsed();
     qDebug() << "parallel " << (float)timeParallel/1000000 << " ms";
     qDebug() << "serial/parallel=" << (float)timeSerial/timeParallel;
     imageOut.save("edges-paralell.jpg");
