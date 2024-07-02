2338119a76c1b032d560ccfa0c37d44bd06d30d3
Evgeny <8evgeny@gmail.com>
2024-07-02 11:24:42 +0300

    ++

--- Current/openCL_project_1_min_max/find_min_max/workSerialMatches.cpp
+++ Current/openCL_project_1_min_max/find_min_max/workSerialMatches.cpp
@@ -11,27 +11,75 @@ using namespace cv;
 extern QElapsedTimer eTimer;
 extern quint64 timeSerial;
 
+Mat img; Mat templ; Mat result;
+const char* image_window = "Source Image";
+const char* result_window = "Result window";
+int match_method = 3;
+int max_Trackbar = 5;
+
+void MatchingMethod( int, void* );
+
+
+
 
 void workSerialMatches()
 {
-//    imageIn.save("matches-in.png");
-//    imageTempl.save("matches-templ.png");
-    eTimer.restart();
-//    Mat matIn(imageIn.height(), imageIn.width(), CV_8UC1, (cv::Scalar*)imageIn.scanLine(0));
-    Mat img_source = imread("image", CV_8UC1);
-    imshow("matIn", img_source);
-    Mat img_templ = imread("template", CV_8UC1);
-    imshow("matTempl", img_templ);
-    waitKey(0);
-    Mat matOut;
+    /// Load image and template
+    img = imread("image", 1 );
+    templ = imread("template", 1 );
 
-//    Mat matTempl(imageTempl.height(), imageTempl.width(), CV_8UC1, (cv::Scalar*)imageTempl.scanLine(0));
-//    Mat matOut(imageOut.height(), imageOut.width(), CV_8UC1, (cv::Scalar*)imageOut.scanLine(0));
-    matchTemplate( img_source, img_templ, matOut, TM_SQDIFF);
-    imshow("matOut", matOut);
+    namedWindow( image_window, WINDOW_AUTOSIZE );
+    namedWindow( result_window, WINDOW_AUTOSIZE );
+    moveWindow(image_window, 100,100);
+    moveWindow(result_window, 750, 140);
+    // Create Trackbar (Переклюячение метода вручную)
+    const char* trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
+//    createTrackbar( trackbar_label, image_window, &match_method, max_Trackbar, MatchingMethod );
+
+    eTimer.restart();
+    MatchingMethod( 0, 0 );
+
     waitKey(0);
-//    imageOut = QImage((uchar*) matOut.data, /*matOut.cols*/640, /*matOut.rows*/480, matOut.step, QImage::Format_Grayscale8/*Format_RGB888*/);
+
     timeSerial = eTimer.nsecsElapsed();
 
-//    imageOut.save("matches-serial.png");
+}
+
+void MatchingMethod( int, void* )
+{
+  /// Source image to display
+  Mat img_display;
+  img.copyTo( img_display );
+
+  /// Create the result matrix
+  int result_cols =  img.cols - templ.cols + 1;
+  int result_rows = img.rows - templ.rows + 1;
+
+  result.create( result_cols, result_rows, CV_32FC1 );
+
+  /// Do the Matching and Normalize
+  matchTemplate( img, templ, result, match_method );
+  normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
+
+  /// Localizing the best match with minMaxLoc
+  double minVal; double maxVal; Point minLoc; Point maxLoc;
+  Point matchLoc;
+
+  minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+
+
+  /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+  if( match_method  == TM_SQDIFF || match_method == TM_SQDIFF_NORMED )
+    { matchLoc = minLoc; }
+  else
+    { matchLoc = maxLoc; }
+
+  /// Show me what you got
+  rectangle( img_display, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
+  rectangle( result, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
+
+  imshow( image_window, img_display );
+  imshow( result_window, result );
+
+  return;
 }
