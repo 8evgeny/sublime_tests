6e796e7003a6319495e1c914ea1269382ce2bd38
Evgeny <8evgeny@gmail.com>
2024-07-03 11:49:18 +0300

    +++

--- Current/openCL_project_1_min_max/0_Simple_add/main.cpp
+++ Current/openCL_project_1_min_max/0_Simple_add/main.cpp
@@ -2,7 +2,37 @@
 
 #include <iostream>
 #include <CL/cl.hpp>
- 
+#include <QtCore>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <iterator>
+QElapsedTimer eTimer;
+quint64 timeSerial, timeParallel;
+
+using namespace std;
+
+vector<int> randomVector(size_t size)
+{
+    vector<int> v(size);
+    std::random_device randD;
+    std::mt19937 randMT(randD());
+    std::uniform_int_distribution <int> range(0, 1000000);
+    generate(v.begin(), v.end(), [&]{return range(randMT);});
+    return v;
+}
+
+void vectorsAddSerial(vector<int>&v1,vector<int>&v2,vector<int>&v3)
+{
+    eTimer.restart();
+    for(int i = 0; i<v1.size();++i)
+    {
+        v3[i] = v1[i] + v2[i];
+    }
+    timeSerial = eTimer.nsecsElapsed();
+    cout<<"timeSerial="<<(float)timeSerial/1000<<" mks "<<endl;
+}
+
 int main(){
     //get all platforms (drivers)
     std::vector<cl::Platform> all_platforms;
@@ -21,7 +51,7 @@ int main(){
         std::cout<<" No devices found. Check OpenCL installation!\n";
         exit(1);
     }
-    cl::Device default_device=all_devices[0];
+    cl::Device default_device = all_devices[0];
     std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<<"\n";
  
  
@@ -30,11 +60,19 @@ int main(){
     cl::Program::Sources sources;
  
     // kernel calculates for each element C=A+B
-    std::string kernel_code=
-            "   void kernel simple_add(global const int* A, global const int* B, global int* C){       "
-            "       C[get_global_id(0)]=A[get_global_id(0)]+B[get_global_id(0)];                 "
-            "   }                                                                               ";
-    sources.push_back({kernel_code.c_str(),kernel_code.length()});
+
+    // Load OpenCL program
+    QFile kernelFile("simple_add.cl");
+    kernelFile.open(QIODevice::ReadOnly | QIODevice::Text);
+    std::string kernel_code_from_file = kernelFile.readAll().toStdString();
+    kernelFile.close();
+
+//    std::string kernel_code=
+//            "   void kernel simple_add(global const int* A, global const int* B, global int* C){       "
+//            "       C[get_global_id(0)]=A[get_global_id(0)]+B[get_global_id(0)];                 "
+//            "   }  ";
+
+    sources.push_back({kernel_code_from_file.c_str(), kernel_code_from_file.length()});
  
     cl::Program program(context,sources);
     if(program.build({default_device})!=CL_SUCCESS){
@@ -42,17 +80,28 @@ int main(){
         exit(1);
     }
  
- 
     // create buffers on the device
     cl::Buffer buffer_A(context,CL_MEM_READ_WRITE,sizeof(int)*10);
     cl::Buffer buffer_B(context,CL_MEM_READ_WRITE,sizeof(int)*10);
     cl::Buffer buffer_C(context,CL_MEM_READ_WRITE,sizeof(int)*10);
  
+    vector<int> v1(randomVector(1000000));
+//    copy(v1.begin(), v1.end(), ostream_iterator<int>(cout, " "));
+    cout<<endl;
+    vector<int> v2(randomVector(1000000));
+//    copy(v2.begin(), v2.end(), ostream_iterator<int>(cout, " "));
+    cout<<endl;
+    vector<int> v3(1000000);
+
+
+    vectorsAddSerial(v1,v2,v3);
+//    copy(v3.begin(), v3.end(), ostream_iterator<int>(cout, " "));
+
     int A[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int B[] = {0, 1, 2, 0, 1, 2, 0, 1, 2, 0};
  
     //create queue to which we will push commands for the device.
-    cl::CommandQueue queue(context,default_device);
+    cl::CommandQueue queue(context, default_device);
  
     //write arrays A and B to the device
     queue.enqueueWriteBuffer(buffer_A,CL_TRUE,0,sizeof(int)*10,A);
@@ -73,12 +122,18 @@ int main(){
  
     int C[10];
     //read result C from the device to array C
-    queue.enqueueReadBuffer(buffer_C,CL_TRUE,0,sizeof(int)*10,C);
+    queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, sizeof(int)*10, C);
  
-    std::cout<<" result: \n";
-    for(int i=0;i<10;i++){
+    std::cout<<"result parallel: \n";
+    for(int i=0;i<10;i++)
+    {
         std::cout<<C[i]<<" ";
     }
- 
+    std::cout<<"\n";
     return 0;
 }
+
+
+
+
+
