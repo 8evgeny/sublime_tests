1733daab4523cebda1805e30f2429a16a86d715d
Evgeny <8evgeny@gmail.com>
2024-07-09 22:09:02 +0300

    daemon ++

--- Khadas_aqua/main.cpp
+++ Khadas_aqua/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <stdio.h>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
@@ -8,10 +8,11 @@
 #include <QtCore>
 #include <QDebug>
 #include <mutex>
+#include <fstream>
 
 using namespace std;
 
-const int gpio_pin_RESERV = 6; 	//PIN22
+const int gpio_pin_UF = 6; 	//PIN22
 const int gpio_pin_COLD = 	7; 	//PIN23
 const int gpio_pin_LAMP = 	10;	//PIN29
 const int gpio_pin_HEAT = 	11;	//PIN30
@@ -22,9 +23,11 @@ const int gpio_pin_FOOD =	15;	//PIN35
 float min_temp, max_temp;
 bool heater = false;
 float temperature;
-QTime time_on,time_off;
+QTime time_light_on, time_light_off;
 bool light = true;
 mutex Mut;
+std::ofstream out;          // поток для записи
+
 
 								//PIN37   onewire  измерение температуры
 								//PIN40   GND
@@ -37,6 +40,7 @@ void printTime()
     const auto now = std::chrono::system_clock::now();
     const auto t_c = std::chrono::system_clock::to_time_t(now);
     std::cout << std::put_time(std::localtime(&t_c), "%T  %d.%b.%y \n");
+    out << std::put_time(std::localtime(&t_c), "%T  %d.%b.%y \n");
 }
 
 void
@@ -46,7 +50,7 @@ receiveTemp(float & temperature)
     {
         Mut.lock();
         std::string tmp;
-        const char *cmd = "./tempread.sh";
+        const char *cmd = "/home/khadas/aqua/tempread.sh";
         char buf[BUFSIZ];
         FILE *ptr;
         if ((ptr = popen(cmd, "r")) != NULL)
@@ -63,12 +67,12 @@ receiveTemp(float & temperature)
 }
 
 void
-receiveSettings(float & min_temp, float & max_temp, bool & heater, QTime & time_on, QTime & time_off, bool & light)
+receiveSettings(float & min_temp, float & max_temp, bool & heater, QTime & time_light_on, QTime & time_light_off, bool & light)
 {
     while(1)
     {
         Mut.lock();
-        QSettings settings("config", QSettings::IniFormat);
+        QSettings settings("/home/khadas/aqua/config", QSettings::IniFormat);
         settings.beginGroup("Temperature");
         min_temp = settings.value( "min_temp").toFloat();
         max_temp = settings.value( "max_temp").toFloat();
@@ -76,8 +80,8 @@ receiveSettings(float & min_temp, float & max_temp, bool & heater, QTime & time_
         settings.endGroup();
 
         settings.beginGroup("Light");
-        time_on = QTime::fromString(settings.value( "time_on").toString());
-        time_off = QTime::fromString(settings.value( "time_off").toString());
+        time_light_on = QTime::fromString(settings.value( "time_light_on").toString());
+        time_light_off = QTime::fromString(settings.value( "time_light_off").toString());
         light = settings.value( "light").toBool();
         settings.endGroup();
 
@@ -87,19 +91,21 @@ receiveSettings(float & min_temp, float & max_temp, bool & heater, QTime & time_
 }
 
 void
-handlerLight(bool & light, QTime & time_on, QTime & time_off)
+handlerLight(bool & light, QTime & time_light_on, QTime & time_light_off)
 {
     this_thread::sleep_for(chrono::milliseconds(5000));
     string stateLight = "none";
     while(1)
     {
         Mut.lock();
+        out.open("/home/khadas/aqua/logFile", std::ios::app);
         if (!light)
         {
             digitalWrite(gpio_pin_LAMP, LOW);
             if (stateLight != "OFF")
             {
                 std::cout << "######## light set OFF #######" <<"\t\t";
+                out << "######## light set OFF #######" <<"\t\t";
                 printTime();
                 stateLight = "OFF";
             }
@@ -107,21 +113,24 @@ handlerLight(bool & light, QTime & time_on, QTime & time_off)
         if (light)
         {
             auto timeNow = QTime::currentTime();
-            if ((timeNow > time_on) && (timeNow < time_off) && (stateLight != "ON"))
+            if ((timeNow > time_light_on) && (timeNow < time_light_off) && (stateLight != "ON"))
             {
                 digitalWrite(gpio_pin_LAMP, HIGH);
                 std::cout << "######## light set ON ########"  <<"\t\t";
+                out << "######## light set ON ########"  <<"\t\t";
                 printTime();
                 stateLight = "ON";
             }
-            if (!((timeNow > time_on) && (timeNow < time_off)) && (stateLight != "OFF"))
+            if (!((timeNow > time_light_on) && (timeNow < time_light_off)) && (stateLight != "OFF"))
             {
                 digitalWrite(gpio_pin_LAMP, LOW);
                 std::cout << "######## light set OFF ########"  <<"\t\t";
+                out << "######## light set OFF ########"  <<"\t\t";
                 printTime();
                 stateLight = "OFF";
             }
         }
+        out.close();
         Mut.unlock();
         this_thread::sleep_for(chrono::milliseconds(1000));
     }
@@ -135,10 +144,12 @@ handlerHeater(bool & heater, float & min_temp, float & max_temp)
     while(1)
     {
         Mut.lock();
+        out.open("/home/khadas/aqua/logFile", std::ios::app); // окрываем файл для дозаписи
         if (!heater && stateHeater != "OFF")
         {
             digitalWrite(gpio_pin_HEAT, LOW);
             std::cout << "######## heater set OFF #######" <<"\t\t";
+            out << "######## heater set OFF #######" <<"\t\t";
             printTime();
             stateHeater = "OFF";
         }
@@ -148,18 +159,20 @@ handlerHeater(bool & heater, float & min_temp, float & max_temp)
             {
                 digitalWrite(gpio_pin_HEAT, LOW);
                 std::cout << "######## heater set OFF #######"  <<"\t\t";
+                out << "######## heater set OFF #######"  <<"\t\t";
                 printTime();
                 stateHeater = "OFF";
             }
             if (temperature < min_temp && stateHeater != "ON")
             {
                 digitalWrite(gpio_pin_HEAT, HIGH);
-                cout<<temperature<<endl;
                 std::cout << "######## heater set ON ########"  <<"\t\t";
+                out << "######## heater set ON ########"  <<"\t\t";
                 printTime();
                 stateHeater = "ON";
             }
         }
+        out.close();
         Mut.unlock();
         this_thread::sleep_for(chrono::milliseconds(1080));
     }
@@ -173,7 +186,7 @@ int main ()
 		exit(1);
 	}
 //    system("gpio readall");
-	pinMode(gpio_pin_RESERV, OUTPUT);
+    pinMode(gpio_pin_UF, OUTPUT);
 	pinMode(gpio_pin_COLD, OUTPUT);
 	pinMode(gpio_pin_LAMP, OUTPUT);
 	pinMode(gpio_pin_HEAT, OUTPUT);
@@ -189,63 +202,75 @@ int main ()
             ref(min_temp),
             ref(max_temp),
             ref(heater),
-            ref(time_on),
-            ref(time_off),
+            ref(time_light_on),
+            ref(time_light_off),
             ref(light)
             ).detach();
-    thread (handlerLight, ref(light), ref(time_on), ref(time_off)).detach();
+    thread (handlerLight, ref(light), ref(time_light_on), ref(time_light_off)).detach();
     thread (handlerHeater, ref(heater), ref(min_temp), ref(max_temp)).detach();
 
     this_thread::sleep_for(chrono::milliseconds(3000));
 
     float temperatureNew, min_tempNew, max_tempNew;
     bool heaterNew = false;
-    QTime time_onNew, time_offNew;
+    QTime time_light_on_New, time_light_off_New;
     bool lightNew = false;
 	while(1)
 	{
+        Mut.lock();
+        out.open("/home/khadas/aqua/logFile", std::ios::app);
         if (temperatureNew != temperature)
         {
             std::cout << "temp = " << temperature <<"\t\t\t\t";
+            out << "temp = " << temperature <<"\t\t\t\t";
             printTime();
             temperatureNew = temperature;
         }
         if (min_tempNew != min_temp)
         {
             std::cout << "settig_min_temp = " << min_temp <<"\t\t\t";
+            out << "settig_min_temp = " << min_temp <<"\t\t\t";
             printTime();
             min_tempNew = min_temp;
         }
         if (max_tempNew != max_temp)
         {
             std::cout << "settig_max_temp = " << max_temp <<"\t\t\t";
+            out << "settig_max_temp = " << max_temp <<"\t\t\t";
             printTime();
             max_tempNew = max_temp;
         }
         if (heaterNew != heater)
         {
             std::cout << "settig_heater = " << boolalpha <<heater<<"\t\t\t";
+            out << "settig_heater = " << boolalpha <<heater<<"\t\t\t";
             printTime();
             heaterNew = heater;
         }
-        if (time_onNew != time_on)
+        if (time_light_on_New != time_light_on)
         {
-            std::cout << "settig_light_on = " << time_on.toString("hh:mm").toStdString()<<"\t\t\t";
+            std::cout << "settig_light_on = " << time_light_on.toString("hh:mm").toStdString()<<"\t\t\t";
+            out << "settig_light_on = " << time_light_on.toString("hh:mm").toStdString()<<"\t\t\t";
             printTime();
-            time_onNew = time_on;
+            time_light_on_New = time_light_on;
         }
-        if (time_offNew != time_off)
+        if (time_light_off_New != time_light_off)
         {
-            std::cout << "settig_light_off = " << time_off.toString("hh:mm").toStdString()<<"\t\t";
+            std::cout << "settig_light_off = " << time_light_off.toString("hh:mm").toStdString()<<"\t\t";
+            out << "settig_light_off = " << time_light_off.toString("hh:mm").toStdString()<<"\t\t";
             printTime();
-            time_offNew = time_off;
+            time_light_off_New = time_light_off;
         }
         if (lightNew != light)
         {
             std::cout << "settig_light = " << boolalpha <<light<<"\t\t\t";
+            out << "settig_light = " << boolalpha <<light<<"\t\t\t";
             printTime();
             lightNew = light;
         }
+        out.close();
+        Mut.unlock();
+
         this_thread::sleep_for(chrono::milliseconds(5000));
 
 
@@ -253,7 +278,7 @@ int main ()
 
 
 
-//		digitalWrite(gpio_pin_RESERV, HIGH);
+//		digitalWrite(gpio_pin_UF, HIGH);
 //		printf("PIN_22 ON\n");
 //		delay(1000);
 //		digitalWrite(gpio_pin_COLD, HIGH);
@@ -275,7 +300,7 @@ int main ()
 //		printf("PIN_35 ON\n");
 //		delay(1000);
 
-//		digitalWrite(gpio_pin_RESERV, LOW);
+//		digitalWrite(gpio_pin_UF, LOW);
 //		printf("PIN_22 OFF\n");
 //		delay(1000);
 //		digitalWrite(gpio_pin_COLD, LOW);
