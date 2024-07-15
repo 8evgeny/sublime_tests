7df36485ef9f63fb858e8321da0a453900be6be4
Evgeny <8evgeny@gmail.com>
2024-07-15 13:40:20 +0300

    matching_3

---
+++ Current/openCL_project_1/3_Matching/main.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <QtCore>
+#include <QImage>
+#include <CL/cl.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+#define WORKGROUPSIZE 16
+//#define NUM_ITERATIONS_GPU  100
+
+struct result
+{
+    int xpos, ypos;
+    uint SAD;
+};
+
+void matchesOpenCV();
+void MatchingMethod( int, void* );
+
+int matchesGPU();
+
+inline void calculateNDRange(int size, int groupSize, int *bSize, int *bGroupSize, int *oSize);
+void convertToGrey(QImage &imageIn);
+
+void qimage_to_mat(const QImage& image, cv::OutputArray out);
+void mat_to_qimage(cv::InputArray image, QImage& out);
+void loadDataMatToUchar(uchar *data, cv::Mat &image, int nchannels);
+void ucharToMat(uchar *data,cv::Mat &image);
+
+enum matchMetod
+{
+    TM_SQDIFF = 0,
+    TM_SQDIFF_NORMED,
+    TM_CCORR,
+    TM_CCORR_NORMED,
+    TM_CCOEFF,
+    TM_CCOEFF_NORMED
+};
