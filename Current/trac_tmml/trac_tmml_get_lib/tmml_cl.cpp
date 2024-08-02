18e34337e71e6d5ce344640d48a8f1ed826d347c
Evgeny <8evgeny@gmail.com>
2024-08-02 15:53:55 +0300

    FIX   OK .....

--- Current/trac_tmml/trac_tmml_get_lib/tmml_cl.cpp
+++ Current/trac_tmml/trac_tmml_get_lib/tmml_cl.cpp
@@ -1,5 +1,4 @@
 #include "tmml_cl.hpp"
-#include <random>
 
 using namespace std;
 using namespace cv;
@@ -8,35 +7,29 @@ using namespace cl;
 tmml_cl::tmml_cl(bool& ok, float& min_max_Val0)
 {
     cout <<"START Construktor tmml_cl\n";
-//    heat_gpu();
-
+    img_result = Mat(Size(RESULT_WIDTH, RESULT_HEIGHT), CV_32FC1, Scalar(0));
+    min_max_Val = min_max_Val0;
+    min_max_Val2 = min_max_Val0 * min_max_Val0;
     initDevice(ok);
     if(!ok){cout<< "error initDevice!!!\n"; return;}
     loadAndBuildProgram(ok, string(KERNEL_FILE));
     if(!ok){cout<< "error loadAndBuildProgram!!!\n"; return;}
-    imageData_ptr = make_unique<cl_uchar[]>(WORK_AREA);
-    templateData_ptr = make_unique<cl_uchar[]>(TEMPLATE_AREA);
-    mData_ptr = make_unique<cl_float[]>(RESULT_AREA);
-
-    clInputImg = Buffer(context, CL_MEM_READ_ONLY  | CL_MEM_ALLOC_HOST_PTR, sizeof(unsigned char) * WORK_AREA);
-    clInputTemp = Buffer(context, CL_MEM_READ_ONLY | CL_MEM_ALLOC_HOST_PTR, sizeof(unsigned char) * TEMPLATE_AREA);
-    clmData = Buffer(context,CL_MEM_WRITE_ONLY | CL_MEM_ALLOC_HOST_PTR,sizeof(cl_uint) * RESULT_AREA);
-
+    img_work_buff = Buffer(context, CL_MEM_READ_WRITE  | CL_MEM_ALLOC_HOST_PTR, sizeof(unsigned char) * WORK_AREA);
+    img_temp_buff = Buffer(context, CL_MEM_READ_WRITE | CL_MEM_ALLOC_HOST_PTR, sizeof(unsigned char) * TEMPLATE_AREA);
+    img_result_buff = Buffer(context, CL_MEM_READ_WRITE | CL_MEM_ALLOC_HOST_PTR, sizeof(float) * RESULT_AREA);
     // Kernels
     int clError = 0;
-    clkProcess = Kernel(program, KERNEL_NAME, &clError );
-    if(clError != 0 )
+    tmml_cl_kernel = Kernel(program, KERNEL_NAME, &clError);
+    if(clError != 0)
     {
-        cout << "clError=" << clError << "; KERNEL_NAME=" << KERNEL_NAME << " =============== !!!" << endl;
+        cout << "clError=" << clError << "; KERNEL_tmml_cl_NAME1=" << KERNEL_NAME << " =============== !!!" << endl;
         ok = false;
         return;
-    }// END if (clError != 0 )
-    queue.enqueueWriteBuffer(clmData, CL_TRUE, 0,  sizeof(cl_float) * RESULT_AREA, &mData_ptr[0]);
-
+    } // END if(clError != 0)
     // Init Kernel arguments
-    clkProcess.setArg(0, clInputImg);
-    clkProcess.setArg(1, clInputTemp);
-    clkProcess.setArg(2, clmData);
+    tmml_cl_kernel.setArg(0, img_work_buff);
+    tmml_cl_kernel.setArg(1, img_temp_buff);
+    tmml_cl_kernel.setArg(2, img_result_buff);
     ok = true;
     cout <<"Constructor tmml_cl ok=" << ok << endl;
 }// END tmml_cl::tmml_cl
@@ -48,39 +41,47 @@ tmml_cl::~tmml_cl()
 
 void tmml_cl::work_tmml(const Mat& img_work, const Mat& img_temp, Pix& max_pix )
 {
-    loadDataMatToUchar(imageData_ptr.get(), img_work);
-    loadDataMatToUchar(templateData_ptr.get(), img_temp);
-    queue.enqueueWriteBuffer(clInputImg, CL_TRUE, 0,  sizeof(unsigned char) * WORK_AREA, &imageData_ptr.get()[0]);
-    queue.enqueueWriteBuffer(clInputTemp, CL_TRUE, 0,  sizeof(unsigned char) * TEMPLATE_AREA, &templateData_ptr.get()[0]);
-    NDRange gRM = NDRange(RESULT_WIDTH, RESULT_HEIGHT);
-    queue.enqueueNDRangeKernel(clkProcess, NullRange, gRM, NullRange );
+    int temp_id = 0;
+    for(int y = 0; y < TEMPLATE_HEIGHT; ++y)
+    {
+        for(int x = 0; x < TEMPLATE_WIDTH; ++x)
+        {
+            img_temp_arr[temp_id] = img_temp.at<unsigned char>(y, x);
+            temp_id++;
+        } // END for(int x = 0; x < TEMPLATE_WIDTH; x++)
+    } // END for(int y = 0; y < TEMPLATE_HEIGHT; y++)
+
+    int work_id = 0;
+    for(int y = 0; y < WORK_HEIGHT; ++y)
+    {
+        for(int x = 0; x < WORK_WIDTH; ++x)
+        {
+            img_work_arr[work_id] = img_work.at<unsigned char>(y, x);
+            work_id++;
+        } // END for(int x = 0; x < TEMPLATE_WIDTH; x++)
+    } // END for(int y = 0; y < TEMPLATE_HEIGHT; y++)
+
+    queue.enqueueWriteBuffer(img_work_buff, CL_TRUE, 0, sizeof(unsigned char) * WORK_AREA, &img_work_arr[0]);
+    queue.enqueueWriteBuffer(img_temp_buff, CL_TRUE, 0, sizeof(unsigned char) * TEMPLATE_AREA, &img_temp_arr[0]);
+
+    // Image 1D
+    NDRange gRM = NDRange(RESULT_WIDTH * RESULT_HEIGHT);
+    queue.enqueueNDRangeKernel(tmml_cl_kernel, NullRange, gRM, NullRange );
     queue.finish();
-    queue.enqueueReadBuffer(clmData, CL_TRUE, 0, sizeof(cl_float) * RESULT_AREA, &mData_ptr[0]);
-    Mat img_result_cpu(RESULT_WIDTH, RESULT_HEIGHT, CV_32F, mData_ptr.get());
-
-//float max = mData_ptr.get()[0];
-//for (int i = 0; i < RESULT_AREA; ++i)
-//{
-//    if (mData_ptr.get()[i] > max)
-//        max = mData_ptr.get()[i];
-//}
-//cout<<"max = "<<max <<"\n";
-
-    minMaxLoc(img_result_cpu, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+    queue.enqueueReadBuffer(img_result_buff, CL_TRUE, 0, sizeof(float) * RESULT_AREA, &img_result.data[0]);
+    minMaxLoc(img_result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
     max_pix.x = maxLoc.x;
     max_pix.y = maxLoc.y;
-    max_pix.bright = mData_ptr.get()[maxLoc.y * RESULT_HEIGHT + maxLoc.x];
-
+    max_pix.bright = maxVal;
 }// END work_tmml
 
 #ifdef KernelFromFile
-string tmml_cl::loadKernelFile(const string program)
+string tmml_cl::loadKernelFile(const string & program)
 {
     ifstream f(program);
     return string((istreambuf_iterator<char>(f)), istreambuf_iterator<char>());
 }// END tmml_cl::loadKernelFile
 #endif
-
 void tmml_cl::initDevice(bool & init_OK)
 {
     Platform::get(&all_platforms);
@@ -107,12 +108,11 @@ void tmml_cl::initDevice(bool & init_OK)
 void tmml_cl::loadAndBuildProgram(bool & init_OK, const string & programFile)
 {
     Program::Sources sources;
-
     #ifdef KernelFromFile
         string kernel_source = loadKernelFile(programFile);
     #endif
     #ifndef KernelFromFile
-        string kernel_source = programm_CL_float;
+        string kernel_source = tmml_cl::programm_CL;
     #endif
 
     pair<const char*, ::size_t> src(kernel_source.c_str(), kernel_source.length());
@@ -125,106 +125,5 @@ void tmml_cl::loadAndBuildProgram(bool & init_OK, const string & programFile)
         cout<<" Error building: "<<program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device)<<"\n";
         init_OK = false;
     }// END if(program.build(devices)!=CL_SUCCESS)
+    cout<<"building_CL OK\n";
 }// END loadAndBuildProgram
-
-void tmml_cl::loadDataMatToUchar(unsigned char *data, const Mat &image)
-{
-    int width = image.cols;
-    int height = image.rows;
-    for (int y = 0; y < height; ++y)
-    {
-        int posY = y * width;
-        for (int x = 0 ; x < width ; ++x)
-        {
-            data[posY + x ] = image.at<unsigned char>(y, x);
-        }// END for (int x = 0 ; x < width ; x++)
-    }// END for (int y=0; y < height; y++)
-}// END loadDataMatToUchar
-
-void tmml_cl::uintToMat(const unsigned int *data, Mat &image)
-{
-    int width = image.cols;
-    int height = image.rows;
-    for (int y = 0; y < height; ++y)
-    {
-        int posY = y * width;
-        for (int x = 0 ; x < width ; ++x)
-        {
-            image.at<uint>(y,x) = data[posY + x];
-        }// END for (int x = 0 ; x < width ; ++x)
-    }// END for (int y = 0; y < height; ++y)
-}// END uintToMat
-
-vector<int> randomVector(int size)
-{
-    vector<int> v(size);
-    std::random_device randD;
-    std::mt19937 randMT(randD());
-    std::uniform_int_distribution <int> range(0, 1000000);
-    generate(v.begin(), v.end(), [&]{return range(randMT);});
-    return v;
-}//END vector<int> randomVector(int size)
-void tmml_cl::heat_gpu()
-{
-    cout<<"########################## heat_gpu start ##################################\n";
-    constexpr int vectorSize = 100'000'000;
-    //get all platforms (drivers)
-    vector<Platform> all_platforms;
-    Platform::get(&all_platforms);
-    if(all_platforms.size()==0)
-    {
-        cout<<" No platforms found. Check OpenCL installation!\n";
-    }//END if(all_platforms.size()==0)
-    Platform default_platform=all_platforms[0];
-    cout << "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>()<<"\n";
-    //get default device of the default platform
-    vector<Device> all_devices;
-    default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
-    if(all_devices.size()==0)
-    {
-        cout<<" No devices found. Check OpenCL installation!\n";
-    }//END if(all_devices.size()==0)
-    Device default_device = all_devices[0];
-    cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<<"\n";
-    Context context({default_device});
-    Program::Sources sources;
-
-    // kernel calculates for each element C=A+B
-    // Load OpenCL program
-    std::string kernel_code =
-        "void kernel add_two_vectors(global const int* A, global const int* B, global int* C)"
-        "{ "
-        "   C[get_global_id(0)]=A[get_global_id(0)]+B[get_global_id(0)]; "
-        " } ";
-    sources.push_back({kernel_code.c_str(), kernel_code.length()});
-    Program program(context, sources);
-    if(program.build({default_device})!=CL_SUCCESS)
-    {
-        cout<<" Error building: "<<program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device)<<"\n";
-    }// END if(program.build({default_device})!=CL_SUCCESS)
-    // create buffers on the device
-    Buffer buffer_A(context, CL_MEM_READ_WRITE, sizeof(int) * vectorSize);
-    Buffer buffer_B(context, CL_MEM_READ_WRITE, sizeof(int) * vectorSize);
-    Buffer buffer_C(context, CL_MEM_READ_WRITE, sizeof(int) * vectorSize);
-    vector<int> v1(randomVector(vectorSize));
-//    copy(v1.begin(), v1.end(), ostream_iterator<int>(cout, " "));
-    vector<int> v2(randomVector(vectorSize));
-//    copy(v2.begin(), v2.end(), ostream_iterator<int>(cout, " "));
-    vector<int> v3(vectorSize);
-    //create queue to which we will push commands for the device.
-    CommandQueue queue(context, default_device);
-    //write arrays A and B to the device
-    queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, sizeof(int) * vectorSize, v1.data());
-    queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, sizeof(int) * vectorSize, v2.data());
-    //run the kernel
-    Kernel kernel_add = Kernel(program, "add_two_vectors");
-    kernel_add.setArg(0, buffer_A);
-    kernel_add.setArg(1, buffer_B);
-    kernel_add.setArg(2, buffer_C);
-    queue.enqueueNDRangeKernel(kernel_add, NullRange, NDRange(vectorSize), NullRange);
-    queue.finish();
-    //read result from the device to vector
-    queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, sizeof(int)*vectorSize, v3.data());
-//    copy(v3.begin(), v3.end(), ostream_iterator<int>(cout, " "));
-    cout<<"########################## heat_gpu end ##################################\n";
-}// END void heat_gpu()
--- Current/trac_tmml/trac_tmml_get_lib/tmml_cl.hpp
+++ Current/trac_tmml/trac_tmml_get_lib/tmml_cl.hpp
@@ -17,7 +17,8 @@ const int TEMPLATE_AREA = TEMPLATE_WIDTH * TEMPLATE_HEIGHT;
 const float RESULT_AREA_1 = 1.f / RESULT_AREA;
 const float TEMPLATE_WIDTH_1 = 1.f / TEMPLATE_WIDTH;
 
-#define KERNEL_FILE "tmml_float.cl"
+#define KERNEL_FILE "tmml.cl"
+
 #ifdef SQDIFF_NORMED
     #define KERNEL_NAME "work_cl_2"
 #endif
@@ -41,21 +42,17 @@ class tmml_cl
     tmml_cl(bool& ok, float& min_max_Val0);
     ~tmml_cl();
 
+    cv::Mat img_result;
+    void work_tmml(const cv::Mat & img_work, const cv::Mat & img_temp, Pix & max_pix);
     const Pix max_pix0;
     Pix max_pix = max_pix0;
 
-    void initDevice(bool & init_OK);
-    void loadAndBuildProgram(bool & init_OK, const std::string & programFile);
-    void loadDataMatToUchar(unsigned char *data, const cv::Mat &image);
-    void uintToMat(const unsigned int *data, cv::Mat &image);
-    std::string loadKernelFile(const std::string program);
-    void work_tmml(const cv::Mat& img_work, const cv::Mat& img_temp, Pix& max_pix);
-    std::unique_ptr<cl_float[]> mData_ptr = nullptr;
-    void heat_gpu();
   private:
+    unsigned char img_temp_arr[TEMPLATE_AREA];
+    unsigned char img_work_arr[WORK_AREA];
     std::string kernel_source{""};
-    cl::Kernel clkProcess;
-    cl::Buffer clInputImg, clInputTemp, clInputMaxVal, clmData;
+    cl::Kernel tmml_cl_kernel;
+    cl::Buffer img_work_buff, img_temp_buff, img_result_buff;
     cl::CommandQueue queue;
     cl::Context context;
     cl::Program program;
@@ -66,10 +63,16 @@ class tmml_cl
     std::unique_ptr<cl_uchar[]> imageData_ptr = nullptr;
     std::unique_ptr<cl_uchar[]> templateData_ptr = nullptr;
 
-    double minVal, maxVal;
+    double min_max_Val = 0, min_max_Val2 = 0;
+    double minVal = 0, maxVal = 0;
     cv::Point minLoc, maxLoc;
-    #ifndef KernelFromFile
-    std::string programm_CL_float  //  \n ->  \\n"\n"
+
+    void initDevice(bool & init_OK);
+    void loadAndBuildProgram(bool & init_OK, const std::string & programFile);
+    std::string loadKernelFile(const std::string & program);
+
+#ifndef KernelFromFile
+    const std::string programm_CL //  \n ->  \\n"\n"
     {
         "__constant int TEMPLATE_WIDTH = 48;\n"
         "__constant int TEMPLATE_HEIGHT = 48;\n"
@@ -83,67 +86,39 @@ class tmml_cl
         "__constant int RESULT_AREA = RESULT_WIDTH * RESULT_HEIGHT;\n"
         "__constant int TEMPLATE_AREA = TEMPLATE_WIDTH * TEMPLATE_HEIGHT;\n"
         "__constant float RESULT_AREA_1 = 1.f / RESULT_AREA;\n"
-        "__constant float TEMPLATE_WIDTH_1 = 1.f / TEMPLATE_WIDTH;\n"
         "__constant float TEMPLATE_AREA_1 = 1.f / TEMPLATE_AREA;\n"
         "\n"
-        "__kernel void work_cl_2(__global unsigned char * img_work, __global unsigned char * img_temp, __global float* matchData )\n"
+        "\n"
+        "__kernel void work_cl_6(__global unsigned char * img_work,\n"
+        "                        __global unsigned char * img_temp,\n"
+        "                        __global float * img_result)\n"
         "{\n"
-        "    int result_x = get_global_id(0);\n"
-        "    int result_y = get_global_id(1);\n"
-        "    int iGID = (result_y * RESULT_HEIGHT  + result_x);\n"
-        "\n"
-        "    int diff_roi_temp = 0;\n"
-        "    int sum_roi_temp_2 = 0;\n"
-        "    unsigned char roi = 0;\n"
-        "    unsigned char temp = 0;\n"
-        "    for ( int tmp_y = 0; tmp_y < TEMPLATE_HEIGHT; tmp_y+=4 )\n"
-        "    {\n"
-        "        int roi_y = result_y + tmp_y;\n"
-        "        for ( int tmp_x = 0; tmp_x < TEMPLATE_WIDTH; tmp_x+=4 )\n"
-        "        {\n"
-        "            int tmp_id = tmp_y * TEMPLATE_WIDTH + tmp_x;\n"
-        "            temp = img_temp[tmp_id];\n"
-        "            int roi_x = result_x + tmp_x;\n"
-        "            int roi_id = roi_y * WORK_WIDTH + roi_x;\n"
-        "            roi = img_work[roi_id];\n"
-        "            diff_roi_temp += abs(roi - temp);\n"
-        "            sum_roi_temp_2 += (roi + temp);\n"
-        "        }// END for ( int X = 0; X < TEMPLATE_WIDTH; X +=1 )\n"
-        "    }// END for ( int Y = 0; Y < TEMPLATE_HEIGHT; Y += 1 )\n"
-        "    matchData[ iGID ] = (float) diff_roi_temp / sum_roi_temp_2;\n"
-        "}// END work_cl_2_no_img\n"
-        "\n"
-        "__kernel void work_cl_6(__global unsigned char * img_work, __global unsigned char * img_temp, __global float* matchData )\n"
-        "{\n"
-        "    int result_x = get_global_id(0);\n"
-        "    int result_y = get_global_id(1);\n"
-        "    int iGID = (result_y * RESULT_HEIGHT  + result_x);\n"
-        "\n"
+        "//    int result_x = get_global_id(0);\n"
+        "//    int result_y = get_global_id(1);\n"
+        "    int result_id = get_global_id(0);\n"
+        "    int result_y = result_id / RESULT_WIDTH;\n"
+        "    int result_x = result_id % RESULT_WIDTH;\n"
         "    int sum_roi_temp = 0;\n"
         "    int sum_temp_temp = 0;\n"
-        "    int diff_roi_temp = 0;\n"
         "    int sum_roi_roi = 0;\n"
         "    int sum_roi = 0;\n"
         "    int sum_temp = 0;\n"
-        "    unsigned char roi = 0;\n"
-        "    unsigned char temp = 0;\n"
-        "    for ( int tmp_y = 0; tmp_y < TEMPLATE_HEIGHT; tmp_y+=4 )\n"
+        "    int temp_id = 0;\n"
+        "    for(int tmp_y = 0; tmp_y < TEMPLATE_HEIGHT; ++tmp_y)\n"
         "    {\n"
-        "        int roi_y = result_y + tmp_y;\n"
-        "        for ( int tmp_x = 0; tmp_x < TEMPLATE_WIDTH; tmp_x+=4 )\n"
+        "        int roi_id0 = (result_y + tmp_y) * WORK_WIDTH + result_x;\n"
+        "        for(int tmp_x = 0; tmp_x < TEMPLATE_WIDTH; ++tmp_x)\n"
         "        {\n"
-        "            int tmp_id = tmp_y * TEMPLATE_WIDTH + tmp_x;\n"
-        "            temp = img_temp[tmp_id];\n"
-        "            int roi_x = result_x + tmp_x;\n"
-        "            int roi_id = roi_y * WORK_WIDTH + roi_x;\n"
-        "            roi = img_work[roi_id];\n"
+        "            unsigned char temp = img_temp[temp_id];\n"
+        "            unsigned char roi = img_work[roi_id0 + tmp_x];\n"
         "            sum_roi_temp += roi * temp;\n"
         "            sum_temp_temp += temp * temp;\n"
         "            sum_roi_roi += roi * roi;\n"
         "            sum_roi += roi;\n"
         "            sum_temp += temp;\n"
-        "        }// END for ( int X = 0; X < TEMPLATE_WIDTH; X +=1 )\n"
-        "    }// END for ( int Y = 0; Y < TEMPLATE_HEIGHT; Y += 1 )\n"
+        "            temp_id++;\n"
+        "        } // END for(int tmp_x = 0; tmp_x < TEMPLATE_WIDTH; ++tmp_x)\n"
+        "    } // END for(int tmp_y = 0; tmp_y < TEMPLATE_HEIGHT; ++tmp_y)\n"
         "    const float sum_roi_temp1 = TEMPLATE_AREA_1 * sum_roi_temp;\n"
         "    const float sum_roi1 = TEMPLATE_AREA_1 * sum_roi;\n"
         "    const float sum_temp1 = TEMPLATE_AREA_1 * sum_temp;\n"
@@ -152,15 +127,19 @@ class tmml_cl
         "    const float ch  = sum_roi_temp1 - sum_roi1 * sum_temp1;\n"
         "    const float zn1 = sum_temp_temp1 - sum_temp1 * sum_temp1;\n"
         "    const float zn2 = sum_roi_roi1 - sum_roi1 * sum_roi1;\n"
-        "    matchData[ iGID ] = ch / sqrt(zn1 * zn2);\n"
-        "}// END void work_cl_6\n"
+        "    img_result[result_id] = ch / sqrt(zn1 * zn2);\n"
+        "} // END void work_cl_6\n"
         "\n"
-        "__kernel void work_cl_8(__global unsigned char * img_work, __global unsigned char * img_temp, __global float* matchData )\n"
+        "\n"
+        "__kernel void work_cl_8(__global unsigned char * img_work,\n"
+        "                        __global unsigned char * img_temp,\n"
+        "                        __global float * img_result)\n"
         "{\n"
-        "    int result_x = get_global_id(0);\n"
-        "    int result_y = get_global_id(1);\n"
-        "    int iGID = (result_y * RESULT_HEIGHT  + result_x);\n"
-        "\n"
+        "//    int result_x = get_global_id(0);\n"
+        "//    int result_y = get_global_id(1);\n"
+        "    int result_id = get_global_id(0);\n"
+        "    int result_y = result_id / RESULT_WIDTH;\n"
+        "    int result_x = result_id % RESULT_WIDTH;\n"
         "    int sum_roi_temp = 0;\n"
         "    int sum_roi_temp_2 = 0;\n"
         "    int sum_temp_temp = 0;\n"
@@ -168,19 +147,14 @@ class tmml_cl
         "    int sum_roi_roi = 0;\n"
         "    int sum_roi = 0;\n"
         "    int sum_temp = 0;\n"
-        "    float dev_result_array_bright0 = 0;\n"
-        "    unsigned char roi = 0;\n"
-        "    unsigned char temp = 0;\n"
-        "    for ( int tmp_y = 0; tmp_y < TEMPLATE_HEIGHT; tmp_y+=4 )\n"
+        "    int temp_id = 0;\n"
+        "    for(int tmp_y = 0; tmp_y < TEMPLATE_HEIGHT; ++tmp_y)\n"
         "    {\n"
-        "        int roi_y = result_y + tmp_y;\n"
-        "        for ( int tmp_x = 0; tmp_x < TEMPLATE_WIDTH; tmp_x+=4 )\n"
+        "        int roi_id0 = (result_y + tmp_y) * WORK_WIDTH + result_x;\n"
+        "        for(int tmp_x = 0; tmp_x < TEMPLATE_WIDTH; ++tmp_x)\n"
         "        {\n"
-        "            int tmp_id = tmp_y * TEMPLATE_WIDTH + tmp_x;\n"
-        "            temp = img_temp[tmp_id];\n"
-        "            int roi_x = result_x + tmp_x;\n"
-        "            int roi_id = roi_y * WORK_WIDTH + roi_x;\n"
-        "            roi = img_work[roi_id];\n"
+        "            int temp = img_temp[temp_id];\n"
+        "            int roi = img_work[roi_id0 + tmp_x];\n"
         "            sum_roi_temp += roi * temp;\n"
         "            sum_temp_temp += temp * temp;\n"
         "            sum_roi_roi += roi * roi;\n"
@@ -188,9 +162,9 @@ class tmml_cl
         "            sum_temp += temp;\n"
         "            diff_roi_temp += abs(roi - temp);\n"
         "            sum_roi_temp_2 += (roi + temp);\n"
-        "        }// END for ( int tmp_x = 0; tmp_x < TEMPLATE_WIDTH; ++tmp_x )\n"
-        "    }// END for ( int tmp_y = 0; tmp_y < TEMPLATE_HEIGHT; ++tmp_y )\n"
-        "\n"
+        "            temp_id++;\n"
+        "        } // END for(int tmp_x = 0; tmp_x < TEMPLATE_WIDTH; ++tmp_x)\n"
+        "    } // END for(int tmp_y = 0; tmp_y < TEMPLATE_HEIGHT; ++tmp_y)\n"
         "    const float sum_roi_temp1 = TEMPLATE_AREA_1 * sum_roi_temp;\n"
         "    const float sum_roi1 = TEMPLATE_AREA_1 * sum_roi;\n"
         "    const float sum_temp1 = TEMPLATE_AREA_1 * sum_temp;\n"
@@ -199,11 +173,10 @@ class tmml_cl
         "    const float ch  = sum_roi_temp1 - sum_roi1 * sum_temp1;\n"
         "    const float zn1 = sum_temp_temp1 - sum_temp1 * sum_temp1;\n"
         "    const float zn2 = sum_roi_roi1 - sum_roi1 * sum_roi1;\n"
-        "    matchData[ iGID ] = ch / sqrt(zn1 * zn2) - (float)diff_roi_temp / sum_roi_temp_2;\n"
-        "}// END tmml_cl_8\n"
+        "    img_result[result_id] = ch / sqrt(zn1 * zn2) - (float)diff_roi_temp / sum_roi_temp_2;\n"
+        "} // END tmml_cl_8\n"
     };
-    #endif
+#endif
+
 }; // END tmml_cl
-
-
 
