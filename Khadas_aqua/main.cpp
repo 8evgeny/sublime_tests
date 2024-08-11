5227e5c283409fb002b974d001860b57f2972817
Evgeny <8evgeny@gmail.com>
2024-08-11 17:14:16 +0300

    fix aqua

--- Khadas_aqua/main.cpp
+++ Khadas_aqua/main.cpp
@@ -372,12 +372,18 @@ int main ()
     }
 //    system("gpio readall");
     pinMode(gpio_pin_UF, OUTPUT);
-    pinMode(gpio_pin_COLD, OUTPUT);
-    pinMode(gpio_pin_LAMP, OUTPUT);
-    pinMode(gpio_pin_HEAT, OUTPUT);
     pinMode(gpio_pin_PUMP_AIR, OUTPUT);
     pinMode(gpio_pin_FOOD, OUTPUT);
+    pinMode(gpio_pin_HEAT, OUTPUT);
+    pinMode(gpio_pin_LAMP, OUTPUT);
+    pinMode(gpio_pin_COLD, OUTPUT);
     pinMode(gpio_pin_REZERV, OUTPUT);
+
+    digitalWrite(gpio_pin_UF, LOW);
+    digitalWrite(gpio_pin_PUMP_AIR, LOW);
+    digitalWrite(gpio_pin_FOOD, LOW);
+    digitalWrite(gpio_pin_HEAT, LOW);
+    digitalWrite(gpio_pin_LAMP, LOW);
 
     this_thread::sleep_for(chrono::milliseconds(3000));
     thread (receiveTemp, ref(temperature)).detach();
