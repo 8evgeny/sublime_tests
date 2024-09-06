be210784611311cb27a82977ff9536a9eeff731e
Evgeny <8evgeny@gmail.com>
2024-09-06 18:27:26 +0300

    FIX

---
+++ Current/inference_yolo_8_in_RK3588/3_convert_coco_onnx_rknn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /home/user/anaconda3/etc/profile.d/conda.sh
+conda activate fastdeploy
+python examples/yolov8_coco/python/convert.py best.onnx rk3588
+
+
+
+
+#`<onnx_model>`: Specify ONNX model path.
+#`<TARGET_PLATFORM>`: Specify NPU platform name. Such as 'rk3588'.
+#`<dtype>(optional)`: Specify as `i8`, `u8` or `fp`. `i8`/`u8` for doing quantization, `fp` for no quantization. Default is `i8`.
+#`<output_rknn_path>(optional)`: Specify save path for the RKNN model, default save in the same directory as ONNX model with name `yolov8.rknn`
