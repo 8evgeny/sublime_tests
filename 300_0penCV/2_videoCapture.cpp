42a91852fe96e63cebf52cf8b5c3dcf22c4b6476
Evgeny <8evgeny@gmail.com>
2024-09-23 12:18:47 +0300

    300

--- 300_0penCV/2_videoCapture.cpp
+++ 300_0penCV/2_videoCapture.cpp
@@ -9,8 +9,8 @@ int main(int argc, char **argv)
     const char win1[]="Захват1", win2[]="Запись1";
     const char win3[]="Захват2", win4[]="Запись2";
     double fps=25; // Число кадров в секунду
-    char file_out[]="../recorded.avi";
-    char file_out2[]="../recorded2.avi";
+    char file_out[]="recorded.avi";
+//    char file_out2[]="../recorded2.avi";
     VideoCapture inVid(0); // Открыть камеру по умолчанию
 //    VideoCapture inVid2(1);
 
@@ -23,17 +23,17 @@ int main(int argc, char **argv)
     int height = (int)inVid.get(cv::CAP_PROP_FRAME_HEIGHT);
 //    int width2 = (int)inVid2.get(cv::CAP_PROP_FRAME_WIDTH);
 //    int height2 = (int)inVid2.get(cv::CAP_PROP_FRAME_HEIGHT);
-    VideoWriter recVid(file_out,
-           cv::VideoWriter::fourcc('D', 'I', 'V', 'X'),
-//                       CV_FOURCC('P','I','M','1')    = MPEG-1 codec
-//                       CV_FOURCC('M','J','P','G')    = motion-jpeg codec
-//                       CV_FOURCC('M', 'P', '4', '2'),// = MPEG-4.2 codec
-//                       CV_FOURCC('D', 'I', 'V', '3') = MPEG-4.3 codec
-//                       CV_FOURCC('D', 'I', 'V', 'X') = MPEG-4 codec
-//                       CV_FOURCC('U', '2', '6', '3'),// = H263 codec
-//                       CV_FOURCC('I', '2', '6', '3') = H263I codec
-//                       CV_FOURCC('F', 'L', 'V', '1') = FLV1 codec
-            fps, Size(width, height));
+//    VideoWriter recVid(file_out,
+//           cv::VideoWriter::fourcc('D', 'I', 'V', 'X'),
+////                       CV_FOURCC('P','I','M','1')    = MPEG-1 codec
+////                       CV_FOURCC('M','J','P','G')    = motion-jpeg codec
+////                       CV_FOURCC('M', 'P', '4', '2'),// = MPEG-4.2 codec
+////                       CV_FOURCC('D', 'I', 'V', '3') = MPEG-4.3 codec
+////                       CV_FOURCC('D', 'I', 'V', 'X') = MPEG-4 codec
+////                       CV_FOURCC('U', '2', '6', '3'),// = H263 codec
+////                       CV_FOURCC('I', '2', '6', '3') = H263I codec
+////                       CV_FOURCC('F', 'L', 'V', '1') = FLV1 codec
+//            fps, Size(width, height));
 //    VideoWriter recVid2(file_out2,
 //           cv::VideoWriter::fourcc('D', 'I', 'V', 'X'),
 ////                       CV_FOURCC('P','I','M','1')    = MPEG-1 codec
@@ -45,10 +45,10 @@ int main(int argc, char **argv)
 ////                       CV_FOURCC('I', '2', '6', '3') = H263I codec
 ////                       CV_FOURCC('F', 'L', 'V', '1') = FLV1 codec
 //            fps, Size(width2, height2));
-    if (!recVid.isOpened()) {
-        cout << "Ошибка! Видеофайл не открыт...\n";
-        return -1;
-    }
+//    if (!recVid.isOpened()) {
+//        cout << "Ошибка! Видеофайл не открыт...\n";
+//        return -1;
+//    }
 //    if (!recVid2.isOpened()) {
 //        cout << "Ошибка! Видеофайл не открыт...\n";
 //        return -1;
@@ -56,11 +56,11 @@ int main(int argc, char **argv)
     // Создаем два окна: для исходного и конечного видео
      namedWindow(win1);
 //     namedWindow(win2);
-     namedWindow(win3);
+//     namedWindow(win3);
 //     namedWindow(win4);
      moveWindow(win1, 300, 0);
 //     moveWindow(win2, 1000, 0);
-     moveWindow(win3, 300, 800);
+//     moveWindow(win3, 300, 800);
 //     moveWindow(win4, 1000, 800);
      while (true) {
         // Читаем кадр с камеры (захват и декодирование)
