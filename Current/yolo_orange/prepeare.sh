0f8bd66c4132ab2c87b320447a34c0d09f55e351
Evgeny <8evgeny@gmail.com>
2024-10-08 17:06:38 +0300

    FIX

--- Current/yolo_orange/prepeare.sh
+++ Current/yolo_orange/prepeare.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
-sshfs user@192.168.1.18:/home/user/Desktop/Desktop_sshfs /home/user/Desktop/Orange_sshfs
+#sshfs user@192.168.1.18:/home/user/Desktop/Desktop_sshfs /home/user/Desktop/Orange_sshfs
+sshfs user@192.168.1.18:/home/user/Desktop/sshfs /home/user/Desktop/Orange_sshfs
 #sshfs user@192.168.1.152:/home/user/Desktop/Desktop_sshfs /home/user/Desktop/Orange_sshfs
