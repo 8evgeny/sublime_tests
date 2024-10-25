63ccef2c7563afb93dbcae39903589c22b95d73d
Evgeny <8evgeny@gmail.com>
2024-10-25 08:03:32 +0300

    time

--- Khadas_aqua/main.cpp
+++ Khadas_aqua/main.cpp
@@ -9,7 +9,10 @@
 #include <QDebug>
 #include <mutex>
 #include <fstream>
-
+#include <sys/time.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
 using namespace std;
 
 const int gpio_pin_UF       = 6;    //PIN22     1_RELE
@@ -532,44 +535,7 @@ int main ()
 
 
     return 0;
-}
-
-#if 0
-https://souptonuts.sourceforge.net/code/sunrise.c.html
-Reference:
- http://prdownloads.sourceforge.net/souptonuts/working_with_time.tar.gz?download
- http://www.srrb.noaa.gov/highlights/sunrise/sunrise.html
-
-
-Compile:
-
-  gcc -o sunrise -Wall -W -O2 -s -pipe -lm sunrise.c
-
-or for debug output
-
-  gcc -o sunrise -DDEBUG=1 -Wall -W -O2 -s -pipe -lm sunrise.c
-
-          This can also go in a batch job to calculate the next
-          20 days as follows:
-
-            #!/bin/bash
-            lat=39.95
-            long=75.15
-            for (( i=0; i <= 20; i++))
-            do
-             ./sunrise    `date -d "+$i day" "+%Y %m %d"` $lat $long
-            done
-#endif
-
-#ifndef DEBUG
-#define DEBUG 0
-#endif
-
-#include <sys/time.h>
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
+} //END main
 
 double calcSunEqOfCenter(double t);
 
@@ -577,150 +543,148 @@ double calcSunEqOfCenter(double t);
 
 double  degToRad(double angleDeg)
 {
-  return (M_PI * angleDeg / 180.0);
+    return (M_PI * angleDeg / 180.0);
 }
 
 double radToDeg(double angleRad)
 {
-  return (180.0 * angleRad / M_PI);
+    return (180.0 * angleRad / M_PI);
 }
 
 double calcMeanObliquityOfEcliptic(double t)
 {
-  double seconds = 21.448 - t*(46.8150 + t*(0.00059 - t*(0.001813)));
-  double e0 = 23.0 + (26.0 + (seconds/60.0))/60.0;
-  return e0;              // in degrees
+    double seconds = 21.448 - t*(46.8150 + t*(0.00059 - t*(0.001813)));
+    double e0 = 23.0 + (26.0 + (seconds/60.0))/60.0;
+    return e0;              // in degrees
 }
 
 double calcGeomMeanLongSun(double t)
 {
-  double L = 280.46646 + t * (36000.76983 + 0.0003032 * t);
-  while( (int) L >  360 )
+    double L = 280.46646 + t * (36000.76983 + 0.0003032 * t);
+    while( (int) L >  360 )
     {
-      L -= 360.0;
+        L -= 360.0;
     }
-  while(  L <  0)
+    while(  L <  0)
     {
-      L += 360.0;
+        L += 360.0;
     }
-
-  return L;              // in degrees
+    return L;              // in degrees
 }
-
 
 double calcObliquityCorrection(double t)
 {
-  double e0 = calcMeanObliquityOfEcliptic(t);
-  double omega = 125.04 - 1934.136 * t;
-  double e = e0 + 0.00256 * cos(degToRad(omega));
-  return e;               // in degrees
+    double e0 = calcMeanObliquityOfEcliptic(t);
+    double omega = 125.04 - 1934.136 * t;
+    double e = e0 + 0.00256 * cos(degToRad(omega));
+    return e;               // in degrees
 }
 
 double calcEccentricityEarthOrbit(double t)
 {
-  double e = 0.016708634 - t * (0.000042037 + 0.0000001267 * t);
-  return e;               // unitless
+    double e = 0.016708634 - t * (0.000042037 + 0.0000001267 * t);
+    return e;               // unitless
 }
 
 double calcGeomMeanAnomalySun(double t)
 {
-  double M = 357.52911 + t * (35999.05029 - 0.0001537 * t);
-  return M;               // in degrees
+    double M = 357.52911 + t * (35999.05029 - 0.0001537 * t);
+    return M;               // in degrees
 }
 
 double calcEquationOfTime(double t)
 {
-  double epsilon = calcObliquityCorrection(t);
-  double  l0 = calcGeomMeanLongSun(t);
-  double e = calcEccentricityEarthOrbit(t);
-  double m = calcGeomMeanAnomalySun(t);
-  double y = tan(degToRad(epsilon)/2.0);
-  y *= y;
-  double sin2l0 = sin(2.0 * degToRad(l0));
-  double sinm   = sin(degToRad(m));
-  double cos2l0 = cos(2.0 * degToRad(l0));
-  double sin4l0 = sin(4.0 * degToRad(l0));
-  double sin2m  = sin(2.0 * degToRad(m));
-  double Etime = y * sin2l0 - 2.0 * e * sinm + 4.0 * e * y * sinm * cos2l0
+    double epsilon = calcObliquityCorrection(t);
+    double  l0 = calcGeomMeanLongSun(t);
+    double e = calcEccentricityEarthOrbit(t);
+    double m = calcGeomMeanAnomalySun(t);
+    double y = tan(degToRad(epsilon)/2.0);
+    y *= y;
+    double sin2l0 = sin(2.0 * degToRad(l0));
+    double sinm   = sin(degToRad(m));
+    double cos2l0 = cos(2.0 * degToRad(l0));
+    double sin4l0 = sin(4.0 * degToRad(l0));
+    double sin2m  = sin(2.0 * degToRad(m));
+    double Etime = y * sin2l0 - 2.0 * e * sinm + 4.0 * e * y * sinm * cos2l0
                 - 0.5 * y * y * sin4l0 - 1.25 * e * e * sin2m;
-
-  return radToDeg(Etime)*4.0;	// in minutes of time
+    return radToDeg(Etime)*4.0;	// in minutes of time
 }
 
 double calcTimeJulianCent(double jd)
 {
-  double T = ( jd - 2451545.0)/36525.0;
-  return T;
+    double T = ( jd - 2451545.0)/36525.0;
+    return T;
 }
 
 double calcSunTrueLong(double t)
 {
-  double l0 = calcGeomMeanLongSun(t);
-  double c = calcSunEqOfCenter(t);
-  double O = l0 + c;
-  return O;               // in degrees
+    double l0 = calcGeomMeanLongSun(t);
+    double c = calcSunEqOfCenter(t);
+    double O = l0 + c;
+    return O;               // in degrees
 }
 
 double calcSunApparentLong(double t)
 {
-  double o = calcSunTrueLong(t);
-  double  omega = 125.04 - 1934.136 * t;
-  double  lambda = o - 0.00569 - 0.00478 * sin(degToRad(omega));
-  return lambda;          // in degrees
+    double o = calcSunTrueLong(t);
+    double  omega = 125.04 - 1934.136 * t;
+    double  lambda = o - 0.00569 - 0.00478 * sin(degToRad(omega));
+    return lambda;          // in degrees
 }
 
 double calcSunDeclination(double t)
 {
-  double e = calcObliquityCorrection(t);
-  double lambda = calcSunApparentLong(t);
-  double sint = sin(degToRad(e)) * sin(degToRad(lambda));
-  double theta = radToDeg(asin(sint));
-  return theta;           // in degrees
+    double e = calcObliquityCorrection(t);
+    double lambda = calcSunApparentLong(t);
+    double sint = sin(degToRad(e)) * sin(degToRad(lambda));
+    double theta = radToDeg(asin(sint));
+    return theta;           // in degrees
 }
 
 double calcHourAngleSunrise(double lat, double solarDec)
 {
-  double latRad = degToRad(lat);
-  double sdRad  = degToRad(solarDec);
-  double HA = (acos(cos(degToRad(90.833))/(cos(latRad)*cos(sdRad))-tan(latRad) * tan(sdRad)));
-  return HA;              // in radians
+    double latRad = degToRad(lat);
+    double sdRad  = degToRad(solarDec);
+    double HA = (acos(cos(degToRad(90.833))/(cos(latRad)*cos(sdRad))-tan(latRad) * tan(sdRad)));
+    return HA;              // in radians
 }
 
 double calcHourAngleSunset(double lat, double solarDec)
 {
-  double latRad = degToRad(lat);
-  double sdRad  = degToRad(solarDec);
-  double HA = (acos(cos(degToRad(90.833))/(cos(latRad)*cos(sdRad))-tan(latRad) * tan(sdRad)));
-  return -HA;              // in radians
+    double latRad = degToRad(lat);
+    double sdRad  = degToRad(solarDec);
+    double HA = (acos(cos(degToRad(90.833))/(cos(latRad)*cos(sdRad))-tan(latRad) * tan(sdRad)));
+    return -HA;              // in radians
 }
 
 double calcJD(int year,int month,int day)
+{
+    if (month <= 2)
     {
-        if (month <= 2) {
-            year -= 1;
-            month += 12;
-        }
-        int A = floor(year/100);
-        int B = 2 - A + floor(A/4);
-        double JD = floor(365.25*(year + 4716)) + floor(30.6001*(month+1)) + day + B - 1524.5;
-        return JD;
+        year -= 1;
+        month += 12;
     }
+    int A = floor(year/100);
+    int B = 2 - A + floor(A/4);
+    double JD = floor(365.25*(year + 4716)) + floor(30.6001*(month+1)) + day + B - 1524.5;
+    return JD;
+}
 
 double calcJDFromJulianCent(double t)
 {
-  double JD = t * 36525.0 + 2451545.0;
-  return JD;
+    double JD = t * 36525.0 + 2451545.0;
+    return JD;
 }
 
 double calcSunEqOfCenter(double t)
 {
-        double m = calcGeomMeanAnomalySun(t);
-        double mrad = degToRad(m);
-        double sinm = sin(mrad);
-        double sin2m = sin(mrad+mrad);
-        double sin3m = sin(mrad+mrad+mrad);
-        double C = sinm * (1.914602 - t * (0.004817 + 0.000014 * t)) + sin2m * (0.019993 - 0.000101 * t) + sin3m * 0.000289;
-        return C;		// in degrees
+    double m = calcGeomMeanAnomalySun(t);
+    double mrad = degToRad(m);
+    double sinm = sin(mrad);
+    double sin2m = sin(mrad+mrad);
+    double sin3m = sin(mrad+mrad+mrad);
+    double C = sinm * (1.914602 - t * (0.004817 + 0.000014 * t)) + sin2m * (0.019993 - 0.000101 * t) + sin3m * 0.000289;
+    return C;		// in degrees
 }
 
 double calcSunriseUTC(double JD, double latitude, double longitude)
@@ -764,87 +728,42 @@ double calcSunsetUTC(double JD, double latitude, double longitude)
     return timeUTC;
 }
 
-//int main(int argc, char **argv)
 void sunset(int year, int  month, int day)
 {
-  time_t seconds;
-  time_t tseconds;
-  struct tm  *ptm=NULL;
-  struct tm  tm;
-//  int year=2024,month=10,day=31,dst=-1;
-  char buffer[30];
-  float JD=calcJD(year,month,day);
-  double latitude = 55.751244;  //convert to just degrees.  No min/sec
-  double longitude = 37.618423;
-  int dst = -1;
-//  if(argc == 4)
-//  {
-//    year=atoi(argv[1]);
-//    month=atoi(argv[2]);
-//    day=atoi(argv[3]);
-//    latitude = atof(argv[4]);
-//    longitude = atof(argv[5]);
-//    JD = calcJD(year,month,day);
+    time_t seconds;
+    time_t tseconds;
+    struct tm  *ptm=NULL;
+    struct tm  tm;
+    char buffer[30];
+    float JD=calcJD(year,month,day);
+    double latitude = 55.751244;  //convert to just degrees.  No min/sec
+    double longitude = 37.618423;
+    int dst = -1;
+    JD = calcJD(year,month,day);
+    tm.tm_year= year-1900;
+    tm.tm_mon=month-1;  /* Jan = 0, Feb = 1,.. Dec = 11 */
+    tm.tm_mday=day;
+    tm.tm_hour=0;
+    tm.tm_min=0;
+    tm.tm_sec=0;
+    tm.tm_isdst=-1;
+    seconds = mktime(&tm);
+    int delta;
+    dst=tm.tm_isdst;
+    ptm = gmtime ( &seconds );
 
-//  } else {
-//  printf("\nUsage: (note convert latitude and longitude to degrees) \n");
-//  printf("./sunrise year month date latitude longitude \n\n");
-//  printf(" Latitude and longitude must be in degrees and or fraction of degrees. Not min sec\n");
-//  printf("\n US Listings Of Latitude and Longitude\n");
-//  printf("    http://www.census.gov/geo/www/tiger/latlng.txt\n");
-//  printf("    But use positive values for longitude for above listing\n");
+    delta= ptm->tm_hour + 5; //Изменил
 
-//  printf("\n Example: (Just outside Philadelphia PA, USA)\n\n");
-//  printf("./sunrise 2004 8 21 39.95 75.15 \n");
-//  printf("./sunrise `date \"+%cY %cm %cd\"` 39.95 75.15\n\n",'%','%','%');
-//  }
-JD = calcJD(year,month,day);
- if(DEBUG)
-  printf("Julian Date  %f \n",JD);
- if(DEBUG)
-  printf("Sunrise timeUTC %lf \n", calcSunriseUTC( JD,  latitude,  longitude));
- if(DEBUG)
-  printf("Sunset  timeUTC %lf \n", calcSunsetUTC( JD,  latitude,  longitude));
+    tseconds = seconds;
+    seconds = seconds + calcSunriseUTC( JD,  latitude,  longitude) * 60;
+    seconds = seconds - delta * 3600;
+    strftime(buffer,30,"%T\n",localtime(&seconds));
+    printf("%s",buffer);
 
-  tm.tm_year= year-1900;
-  tm.tm_mon=month-1;  /* Jan = 0, Feb = 1,.. Dec = 11 */
-  tm.tm_mday=day;
-  tm.tm_hour=0;
-  tm.tm_min=0;
-  tm.tm_sec=0;
-  tm.tm_isdst=-1;
+    seconds = tseconds;
+    seconds += calcSunsetUTC( JD,  latitude,  longitude) * 60;
+    seconds = seconds - delta * 3600;
+    strftime(buffer,30,"%T\n",localtime(&seconds));
+    printf("%s",buffer);
 
-  seconds = mktime(&tm);
-  if(DEBUG)
-   printf("Number of seconds %ld\n",seconds);
-  if(DEBUG)
-   printf("Number of year %d\n",year);
-  int delta;
-
-  dst=tm.tm_isdst;
-  ptm = gmtime ( &seconds );
-
-  delta= ptm->tm_hour + 5; //Изменил
-
-  if(DEBUG)
-    printf("delta=%d dst=%d\n",delta,dst);
-
-  tseconds= seconds;
-  if(DEBUG)
-   printf("Number of seconds %ld\n",seconds);
-  seconds= seconds + calcSunriseUTC( JD,  latitude,  longitude)*60;
-  seconds= seconds - delta*3600;
-
-//  strftime(buffer,30,"%m-%d-%Y  %T",localtime(&seconds));
-//  printf("Sunrise  %s   ",buffer);
-strftime(buffer,30,"%T\n",localtime(&seconds));
-printf("%s",buffer);
-  seconds=tseconds;
-  seconds+=calcSunsetUTC( JD,  latitude,  longitude)*60;
-  seconds= seconds - delta*3600;
-
-  strftime(buffer,30,"%m-%d-%Y  %T",localtime(&seconds));
-//  printf("Sunset %s\n",buffer);
-
-//  return 0;
 }
