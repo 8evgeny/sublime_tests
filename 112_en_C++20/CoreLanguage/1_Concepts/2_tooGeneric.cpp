2b4d330e30e347dd3f5b8300c41bd1750e61e418
Evgeny <8evgeny@gmail.com>
2024-11-23 22:31:45 +0300

    ++

--- 112_en_C++20/CoreLanguage/1_Concepts/2_tooGeneric.cpp
+++ 112_en_C++20/CoreLanguage/1_Concepts/2_tooGeneric.cpp
@@ -5,8 +5,8 @@
 
 int main(){
     
-    std::list<int> myList{1, 10, 3, 2, 5};
-    
+//    std::list<int> myList{1, 10, 3, 2, 5};
+    std::vector<int> myList{1, 10, 3, 2, 5};
     std::sort(myList.begin(), myList.end());
     
 }
