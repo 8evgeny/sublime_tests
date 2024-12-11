467f3a86d2654c75df21642843d319d721ed712e
Evgeny <8evgeny@gmail.com>
2024-12-11 11:16:04 +0300

    begin tmml_texture

---
+++ cuda_examples/tmml_texture_memory/tmml_texture.hpp
@@ -0,0 +1,84 @@
+#pragma once
+#ifndef NO_GPU
+    #include <opencv2/core/cuda.hpp>
+#endif // END ifndef NO_GPU
+
+#define _USE_MATH_DEFINES
+#include <opencv2/core/utility.hpp>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/opencv.hpp"
+#include <stdlib.h>
+#include <math.h>
+#include <cmath>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+const int SOURCE_WIDTH = EXT_VAL * TEMPLATE_WIDTH;
+const int WORK_WIDTH = SOURCE_WIDTH - 1;
+const int RESULT_WIDTH = WORK_WIDTH - TEMPLATE_WIDTH + 1; // 192
+const int RESULT_AREA = RESULT_WIDTH * RESULT_WIDTH;
+const int TEMPLATE_AREA = TEMPLATE_WIDTH * TEMPLATE_WIDTH;
+const float RESULT_AREA_1 = 1.f / RESULT_AREA;
+const float TEMPLATE_WIDTH_1 = 1.f / TEMPLATE_WIDTH;
+const float TEMPLATE_AREA_1 = 1.f / TEMPLATE_AREA;
+const float KOEFF2LIB_float = KOEFF2LIB;
+
+#ifdef GPU_AMPERE
+    const int threads_match_temp = 128; // 256
+
+#endif // END ifdef GPU3090
+
+#ifdef GPU_VOLTA
+    const int threads_match_temp = 128;
+#endif  // END ifdef GPU_XAVIER_NX
+
+#ifndef NO_GPU
+    const float threads_match_temp_1 = 1.f / threads_match_temp;
+    // Число нитей CUDA в итерации:
+    const int blocks_match_temp = RESULT_AREA * threads_match_temp_1;
+#endif // END ifndef NO_GPU
+
+    struct Pix
+    {
+        unsigned char x = 0;
+        unsigned char y = 0;
+        float bright = 0;
+    };
+
+class tmml
+{
+  public:
+    tmml(bool& ok, float& min_max_Val0);
+    ~tmml();
+    void cuda_Free();
+
+    const Pix max_pix0;
+    void work_tmml(const cv::Mat& img_work, const cv::Mat& img_temp, Pix& max_pix);
+    Pix max_pix = max_pix0;
+    double maxVal = 0;
+    cv::Mat img_result = cv::Mat(cv::Size(RESULT_WIDTH, RESULT_WIDTH), CV_32FC1, cv::Scalar(0));
+    cv::Mat img_result2 = cv::Mat(cv::Size(RESULT_WIDTH, RESULT_WIDTH), CV_32FC1, cv::Scalar(0));
+    cv::Mat img_result6 = cv::Mat(cv::Size(RESULT_WIDTH, RESULT_WIDTH), CV_32FC1, cv::Scalar(0));
+
+  private:
+    double minVal, min_max_Val;
+    cv::Point minLoc, maxLoc;
+
+#ifndef NO_GPU
+    void cuda_Malloc();
+    void init_matchers();
+    cv::cuda::GpuMat img_work_gpu, img_temp_gpu;
+    unsigned char  img_temp_arr[TEMPLATE_AREA];
+    float error_Val = 0.f, min_max_Val2 = 0.f;
+    int * dev_max_val0 = 0;
+    int * dev_max_val = dev_max_val0;
+    Pix * dev_mp;
+    float * dev_result_array_bright;
+#endif // END ifndef NO_GPU
+}; // END class tmml
