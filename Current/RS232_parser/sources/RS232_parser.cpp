8e5a04623bbccad9224a5fecb6d8e4790bd68d3a
Evgeny <8evgeny@gmail.com>
2024-12-25 11:42:49 +0300

    FIX

---
+++ Current/RS232_parser/sources/RS232_parser.cpp
@@ -0,0 +1,58 @@
+#include <RS232_parser.hpp>
+using namespace std;
+
+RS232_parser::RS232_parser()
+{
+    cout<<"RS232_parser ctor"<<endl;
+}// END RS232_parser()
+
+RS232_parser::~RS232_parser()
+{
+    cout<<"RS232_parser dtor"<<endl;
+}// END ~RS232_parser()
+
+const QString &RS232_parser::getPortName() const
+{
+    return _portName;
+}// END getPortName()
+
+int RS232_parser::getSpeed() const
+{
+    return _speed;
+}// END getSpeed()
+
+uint8_t RS232_parser::getCmdLen() const
+{
+    return cmdLen;
+}//END getCmdLen()
+
+void RS232_parser::parsing()
+{
+    uint8_t buf[getCmdLen()];
+    uint8_t forParsing[getCmdLen()];
+    while(1)
+    {
+        QByteArray requestData;
+        while (serial.waitForReadyRead(1))
+            requestData += serial.read(getCmdLen());
+
+        if (requestData != "")
+        {
+            for (int i = 0;i < getCmdLen(); ++i)
+            {
+                forParsing[i] = requestData[i];
+                buf[i] = requestData[i];
+//                cout<<hex<<(int)buf[i]<<" ";
+            } //
+//            cout<<dec<<endl;
+            printf( "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X "
+                    "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X "
+                    "\n",
+                    buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7],
+                    buf[8], buf[9], buf[10], buf[11], buf[12], buf[13], buf[14], buf[15],
+                    buf[16], buf[17], buf[18], buf[19], buf[20], buf[21], buf[22], buf[23],
+                    buf[24], buf[25], buf[26], buf[27], buf[28], buf[29], buf[30], buf[31]
+                    );
+        }//END if (requestData != "")
+    }
+}// END parsing()
--- Current/RS232_parser/sources/main.cpp
+++ Current/RS232_parser/sources/main.cpp
@@ -1,44 +1,21 @@
-#include <QDebug>
-#include <QSerialPort>
-#include <QSerialPortInfo>
-#include <iostream>
+#include <RS232_parser.hpp>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QSerialPort serial;
-    serial.setPortName("ttyUSB0");
-    serial.setBaudRate(115200);
-    serial.open(QIODevice::ReadOnly);
-    qDebug() << serial.portName();
-    uint8_t buf[32];
-    uint8_t forParsing[32];
-    for(;;)
-    {
-        QByteArray requestData;
-        while (serial.waitForReadyRead(1))
-            requestData += serial.read(36);
+    unique_ptr<RS232_parser> p = make_unique<RS232_parser>();
 
-        if (requestData != "")
-        {
-            for (int i = 0;i<32;++i)
-            {
-                forParsing[i] = requestData[i];
-                buf[i] = requestData[i];
-//                cout<<hex<<(int)buf[i]<<" ";
-            }
-//            cout<<dec<<endl;
-            printf("%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X "
-            "\n",
-            buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7], buf[8], buf[9], buf[10], buf[11], buf[12], buf[13], buf[14], buf[15],
-            buf[16], buf[17], buf[18], buf[19], buf[20], buf[21], buf[22], buf[23], buf[24], buf[25], buf[26], buf[27], buf[28], buf[29], buf[30], buf[31]
-            );
-        }
-    }
+    p->serial.setPortName(p->getPortName());
+    p->serial.setBaudRate(p->getSpeed());
+
+    p->serial.open(QIODevice::ReadOnly);
+    qDebug() << p->serial.portName();
+
+    p->parsing();
 
 return 0;
-}
+}// END while(1)
 
 
 
