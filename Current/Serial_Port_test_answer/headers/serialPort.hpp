231acdb3045cb2706d9465e85da0f3c7ac37369a
Evgeny <8evgeny@gmail.com>
2025-03-06 17:22:33 +0300

    answer to 485

--- Current/Serial_Port_test_answer/headers/serialPort.hpp
+++ Current/Serial_Port_test_answer/headers/serialPort.hpp
@@ -10,16 +10,19 @@ class UART
 public:
     UART();
     ~UART();
+    void printDataFromPort(uint8_t num_byte, std::string & data);
+    std::string array_uint8_to_string(uint8_t * request, int num);
 
-    const int getSpeed() const;
+//    const int getSpeed() const;
     void work();
 
-    void Read();
+//    void Read();
 
 private:
     std::unique_ptr<LibSerial::SerialPort> _serial_ptr = nullptr;
     bool _port_open_OK = false;
-    const std::string _portName = "/dev/ttyUSB0";
+    const std::string _portName = "/dev/ttyUSB1";
+    uint8_t _readBuf[512];
 
 };
 
--- Current/Serial_Port_test_answer/sources/serialPort.cpp
+++ Current/Serial_Port_test_answer/sources/serialPort.cpp
@@ -45,57 +45,63 @@ UART::UART()
 
 
 }// END UART()
+#include <sstream>
+
+string UART::array_uint8_to_string(uint8_t * request, int num)
+{
+    std::ostringstream conv;
+    for (int i = 0; i < num; ++i)
+    {
+        conv << request[i];
+    }//END for (int i = 0; i < num; ++i)
+    return conv.str();
+}//END array_uint8_to_string(uint8_t * request, int num)
 
 void UART::work()
 {
+    uint8_t request[] = {0xAA, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x7A};
+    uint8_t answer[] = {0xAA, 0x81};
+
+    string good = array_uint8_to_string(request, sizeof(request));
+    string answ = array_uint8_to_string(answer, sizeof(answer));
+    printf( "Good string:   ");
+    printDataFromPort(10, good);
     cout<<"\n===============  port " <<_portName << " begin work  ================\n" <<endl;
+    string data;
     while(1)
     {
-//        this_thread::sleep_for(chrono::milliseconds(10));
-//        if (serial.isReadable())
-//        {
-//            ba = serial.readAll();
-//            qDebug() << "ba: " << ba;
-//        }
-//        requestData.clear();
-//        while (serial.waitForReadyRead(1))
-//        {
-//            this_thread::sleep_for(chrono::microseconds(1));
-//        }
-//        requestData += serial.read(_cmdLen);
-//        if (requestData != "")
-//        {
-//            for (int i = 0; i < _cmdLen; ++i)
-//            {
-//                _cmdBuf[i] = requestData[i];
-//            } //END for (int i = 0; i < _cmdLen; ++i)
+        this_thread::sleep_for(chrono::milliseconds(10));
+        if(_serial_ptr->IsDataAvailable())
+        {
+            int numByte = _serial_ptr->GetNumberOfBytesAvailable();
+            _serial_ptr->Read(data, numByte);
+            printf( "Received from port:   ");
+            printDataFromPort(numByte, data);
+            if (data == good)
+            {
+                cout <<"read request...\n";
+            }//END if (data == goog_request)
+            cout <<"ansver 0xAA, 0x81\n";
+            _serial_ptr->Write(answ);
 
-//            if (_cmdBuf[1] == 0x90)
-//            {
-//                printDataRS232();
-////                printDataRS232_format();
-//            }
-
-//            if(_cmdBuf[31] == 0x00 && _cmdBuf[1] == 0x90) //Признак команды
-//            {
-//                setCMD(find_cmd());
-//            }//END if(_cmdBuf[31] == 0x00 && _cmdBuf[1] == 0x90)
-
-
-
-//        }//END if (requestData != "")
+        }//END if(_serial_ptr->IsDataAvailable())
     }//END while(1)
+
+
+
+
 }// END work()
 
-//void serialPort::printDataRS232()
-//{
-//    printf( "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X "
-//            "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X "
-//            "\n",
-//            _cmdBuf[0], _cmdBuf[1], _cmdBuf[2], _cmdBuf[3], _cmdBuf[4], _cmdBuf[5], _cmdBuf[6], _cmdBuf[7],
-//            _cmdBuf[8], _cmdBuf[9], _cmdBuf[10], _cmdBuf[11], _cmdBuf[12], _cmdBuf[13], _cmdBuf[14], _cmdBuf[15],
-//            _cmdBuf[16], _cmdBuf[17], _cmdBuf[18], _cmdBuf[19], _cmdBuf[20], _cmdBuf[21], _cmdBuf[22], _cmdBuf[23],
-//            _cmdBuf[24], _cmdBuf[25], _cmdBuf[26], _cmdBuf[27], _cmdBuf[28], _cmdBuf[29], _cmdBuf[30], _cmdBuf[31]
-//            );
-//}
+void UART::printDataFromPort(uint8_t num_byte, string & data)
+{
+    for (int i = 0; i < num_byte; ++i)
+    {
+        _readBuf[i] = data[i];
+    }
+    for (int i = 0; i < num_byte; ++i)
+    {
+        printf( "%02X ", _readBuf[i]);
+    }
+    printf("\n");
+}
 
