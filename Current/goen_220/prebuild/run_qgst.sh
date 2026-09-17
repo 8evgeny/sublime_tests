346772842a12665921e07bac8b60e9434a9faa1d
Evgeny <8evgeny@gmail.com>
2026-09-17 18:09:23 +0300

    ----

--- Current/goen_220/prebuild/run_qgst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set the required environment variables
-export QT_DEBUG_PLUGINS=0
-export QT_PLUGIN_PATH=/home/khadas/qt-projects/plugins
-export GST_PLUGIN_PATH=/home/khadas/gst
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/khadas/gst
-
-./qgst-goen220
